61c851438e8ff38311a8fe621d62abc10344ae48
ashley-eqtr <146321241+ashley-eqtr@users.noreply.github.com>
2023-10-30 14:41:56 +0000

    feature/HWPD-485/text-component-documentation (#18)
    Replaced autodocs with a custom markdown doc for Text component, in order to explain how to use it

--- src/component-library/foundation/Text/Text.stories.tsx
+++ src/component-library/foundation/Text/Text.stories.tsx
@@ -9,8 +9,6 @@ const meta: Meta<typeof Text> = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
 }
 
 export default meta
