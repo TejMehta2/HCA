7094b775ecf07ec3988854696b4e65ce518f24df
ashley-eqtr <146321241+ashley-eqtr@users.noreply.github.com>
2023-10-31 13:58:52 +0000

    documentation/add-mdx-to-scaffold (#21)
    The scaffold must adapt to our needs. In this case we have beein adding detailed component documentation in the form of MDX files which are loaded in Storybook.
    This change adds a default MDX file to work from, with some dummy snippets to get started.

--- src/component-library/scaffold/templates/TemplateComponent.stories.tsx
+++ src/component-library/scaffold/templates/TemplateComponent.stories.tsx
@@ -10,8 +10,6 @@ const meta: Meta<typeof TemplateComponent> = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
 }
 
 export default meta
