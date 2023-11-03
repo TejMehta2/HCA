da99f39adf9898a0b54736a9de69afc7f870714c
kristina_greig <kristina.greig@eqtr.com>
2023-11-03 14:36:47 +0000

    Text Link component and updates to button component docs

---
+++ src/component-library/core-components/TextLink/TextLink.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import TextLink from './TextLink';
+import type { Meta, StoryObj } from '@storybook/react';
+import Icons from '../../foundation/Icons/Icons';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof TextLink> = {
+  title: 'core-components/TextLink',
+  component: TextLink,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+};
+
+export default meta;
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: StoryObj<typeof TextLink> = {
+  args: {
+    children: (
+      <button>
+        <span>TextLink</span> <Icons iconName="iconCross" />
+      </button>
+    ),
+  },
+};
