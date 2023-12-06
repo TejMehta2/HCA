049440c693c0e776610d4230648602ea3c28ac7f
kristina_greig <kristina.greig@eqtr.com>
2023-12-06 17:44:11 +0000

    header plain component and story

---
+++ src/component-library/components/HeaderPlain/HeaderPlain.spec.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import HeaderPlain from './HeaderPlain';
+import { HeaderPlainProps } from './HeaderPlain.types';
+import Text from '../../foundation/Text/Text';
+
+const mockProps: HeaderPlainProps = {
+  subheading: (
+    <Text tag="h3" variation="subheading-1">
+      Optional meta title
+    </Text>
+  ),
+  heading: (
+    <Text tag="h1" variation="display-1">
+      News & articles about healthcare
+    </Text>
+  ),
+  children: (
+    <p>
+      Quis laboris proident sint amet id cillum do dolor in tempor est.
+      Exercitation aute sint tempor eu ut aliquip commodo enim nulla et laborum
+      et culpa minim. Commodo ex laboris pariatur labore nostrud dolore.
+    </p>
+  ),
+};
+
+describe('HeaderPlain', () => {
+  it('Renders heading from props', async () => {
+    const { getByText } = render(<HeaderPlain {...mockProps} />);
+    expect(getByText('News & articles about healthcare')).toBeVisible();
+  });
+});
