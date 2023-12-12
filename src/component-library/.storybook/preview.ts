90eed3ff51d69541fcd501750b0559eb2cfbdce9
kristina_greig <kristina.greig@eqtr.com>
2023-12-12 11:19:09 +0000

    update viewport sizes in storybook

--- src/component-library/.storybook/preview.ts
+++ src/component-library/.storybook/preview.ts
@@ -1,6 +1,37 @@
 import type { Preview } from "@storybook/react";
 import "../globals/index.scss";
 
+const customViewports = {
+  small: {
+    name: 'Small',
+    styles: {
+      width: '390px',
+      height: '844px',
+    },
+  },
+  Medium: {
+    name: 'Medium',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  Large: {
+    name: 'Large',
+    styles: {
+      width: '1180px',
+      height: '820px',
+    },
+  },
+  XLarge: {
+    name: 'XLarge',
+    styles: {
+      width: '1440px',
+      height: '900px',
+    },
+  },
+};
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,6 +44,8 @@ const preview: Preview = {
     options: {
       storySort: { order: ["Foundation", "Core Components", "Components"] },
     },
+    viewport: { viewports: customViewports },
+
   },
 };
 
