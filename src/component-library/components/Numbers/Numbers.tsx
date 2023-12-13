d0f316c4d73e40375653a06c89882ad6445f84ea
MichaelRendall <michael.rendall@eqtr.com>
2023-12-13 14:09:03 +0000

    span

--- src/component-library/components/Numbers/Numbers.tsx
+++ src/component-library/components/Numbers/Numbers.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { NumbersProps } from './Numbers.types';
 import styles from './Numbers.module.scss';
-import Text from '../../foundation/Text/Text';
 
 const Numbers = (props: NumbersProps): JSX.Element => {
   const { number, size = 'large' } = props;
-  return (
-    <span className={`${styles.wrapper} ${styles[size]}`}>
-      {
-        <Text tag="span" variation="body-medium-large">
-          {number}
-        </Text>
-      }
-    </span>
-  );
+  return <span className={`${styles.wrapper} ${styles[size]}`}>{number}</span>;
 };
 
 export default Numbers;
