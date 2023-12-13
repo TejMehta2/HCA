465c8793f3a20efcc9eb29d47bedd732cf4c94b2
MichaelRendall <michael.rendall@eqtr.com>
2023-12-13 13:35:53 +0000

    add span

--- src/component-library/components/Numbers/Numbers.tsx
+++ src/component-library/components/Numbers/Numbers.tsx
@@ -6,13 +6,13 @@ import Text from '../../foundation/Text/Text';
 const Numbers = (props: NumbersProps): JSX.Element => {
   const { number, size = 'large' } = props;
   return (
-    <div className={`${styles.wrapper} ${styles[size]}`}>
+    <span className={`${styles.wrapper} ${styles[size]}`}>
       {
         <Text tag="p" variation="body-medium-large">
           {number}
         </Text>
       }
-    </div>
+    </span>
   );
 };
 
