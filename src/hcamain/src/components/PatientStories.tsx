7fe4483e623cafa87794a0759b91de19fa385fef
Bartek Glomski <bartek.glomski@eqtr.com>
2023-12-14 21:25:18 +0100

    patient stories integration in progress

--- src/hcamain/src/components/PatientStories.tsx
+++ src/hcamain/src/components/PatientStories.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import {
+  Image as JssImage,
+  Link as JssLink,
   Field,
   Text,
-  Link,
   LinkField,
   ImageField,
+  RichText,
 } from '@sitecore-jss/sitecore-jss-nextjs';
-import CarouselCards from 'temp/component-library/components/CarouselCards/CarouselCards';
+import SideScrollingCards from 'temp/component-library/components/SideScrollingCards/SideScrollingCards';
+import CardPatientStories from 'temp/component-library/components/CardPatientStories/CardPatientStories';
 
 type CTAIconFields = {
   SVGMarkup: Field<string>;
 };
 
 interface StoriesFields {
-  Title: Field<string>;
-  Description: Field<string>;
-  Date: Field<string>;
-  Image: ImageField;
+  title: Field<string>;
+  description: Field<string>;
+  date: Field<string>;
+  image: ImageField;
+  link: LinkField;
+  url: string;
 }
 
 interface Fields {
-  Title: Field<string>;
-  Text: Field<string>;
-  CardCTAText: Field<string>;
-  CTAIcon: {
-    Icon: CTAIconFields[];
-  };
-  CTALink: LinkField;
-  stories: {
-    StoriesList: StoriesFields[];
+  data: {
+    item: {
+      title: Field<string>;
+      text: Field<string>;
+      cardCTAText: Field<string>;
+      cTAIcon: {
+        ctaIcon: CTAIconFields[];
+      };
+      cTALink: LinkField;
+      stories: {
+        StoriesList: StoriesFields[];
+      };
+    };
   };
 }
 
@@ -41,10 +50,24 @@ const PatientStoriesDefaultComponent = (
   props: PatientStoriesProps
 ): JSX.Element => (
   <div className={`component ${props.params.styles}`}>
-    <CarouselCards
-      title={<Text field={props.fields.Title} />}
-      link={<Link field={props.fields.CTALink} />}
-    ></CarouselCards>
+    <JssImage field={props.fields.data.item.stories.StoriesList[1].image} />
+    <SideScrollingCards
+      title={<Text field={props.fields.data.item.title} />}
+      link={<JssLink field={props.fields.data.item.cTALink} />}
+      bodyCopy={<RichText field={props.fields.data.item.text} />}
+    >
+      {props.fields.data.item.stories.StoriesList.map((story, index) => (
+        <CardPatientStories
+          key={index}
+          title={<Text field={story.title} tag={'h3'} />}
+          link={
+            <a href={story.url}>{props.fields.data.item.cardCTAText.value} </a>
+          }
+          bodyCopy={<RichText field={story.description} />}
+          image={<JssImage field={story.image} />}
+        ></CardPatientStories>
+      ))}
+    </SideScrollingCards>
   </div>
 );
 
