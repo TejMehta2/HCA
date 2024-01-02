b60aca241bdc3ddf90e477e23110223e10996d93
Carmen Popovici <carmen.popovici@eqtr.com>
2024-01-02 16:54:36 +0200

    Header with image component + Doctify testimonials carousel + thumbnails for components

---
+++ src/hcamain/src/components/DoctifyTestimonialsCarousel.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface TestimonialsFields {
+  Text: Field<string>;
+}
+
+interface DoctifyReviewsFields {
+  Stars: Field<string>;
+  Reviews: Field<string>;
+}
+
+interface Fields {
+  Reviews: DoctifyReviewsFields[];
+  testimonials: {
+    Testimonials: TestimonialsFields[];
+  };
+}
+
+type DoctifyTestimonialsCarouselProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+const DoctifyTestimonialsCarouselDefaultComponent = (
+  props: DoctifyTestimonialsCarouselProps
+): JSX.Element => (
+  <div className={`component ${props.params.styles}`}>
+    <h1>Doctify Testimonials Carousel</h1>
+  </div>
+);
+
+export const Default = (props: DoctifyTestimonialsCarouselProps): JSX.Element => {
+  return <DoctifyTestimonialsCarouselDefaultComponent {...props} />;
+};
---
+++ src/hcamain/src/components/HeaderWithImage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Field, Placeholder, ComponentRendering, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Title: Field<string>;
+  Text: Field<string>;
+  Image: ImageField;
+}
+
+type HeaderWithImageProps = {
+  params: { [key: string]: string };
+  rendering: ComponentRendering;
+  fields: Fields;
+};
+
+const HeaderWithImageDefaultComponent = (
+  props: HeaderWithImageProps
+): JSX.Element => {
+  const phKey = `CTAButton-${props.params.DynamicPlaceholderId}`;
+  return (
+     <div className={`component ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">Header With Image</span>
+      <Placeholder name={phKey} rendering={props.rendering} />
+    </div>
+  </div>
+  );
+};
+
+export const Default = (props: HeaderWithImageProps): JSX.Element => {
+
+  return <HeaderWithImageDefaultComponent {...props} />;
+};
