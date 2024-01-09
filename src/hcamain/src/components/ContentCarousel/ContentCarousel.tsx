3ecbf2a914ef843b10dbba1f3ef3e2605d02fd06
ashley-eqtr <146321241+ashley-eqtr@users.noreply.github.com>
2024-01-09 14:48:24 +0000

    feature/HWPD-454/ContentCarousel-integration (#159)
    * feature/HWPD-454/ContentCarousel-integration
    
    - integrated ContentCarousel component
    - added TODO comments pending updated CMS params types from back end
    
    * feature/HWPD-454/ContentCarousel-integration

--- src/hcamain/src/components/ContentCarousel/ContentCarousel.tsx
+++ src/hcamain/src/components/ContentCarousel/ContentCarousel.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import {
   Field,
   ImageField,
-  Text,
-  RichText,
-  Image,
+  Text as JssText,
+  RichText as JssRichText,
+  Image as JssImage,
 } from '@sitecore-jss/sitecore-jss-nextjs';
+import CarouselContent from '@component-library/site-components/CarouselContent/CarouselContent';
+import Text from '@component-library/foundation/Text/Text';
+import { Theme } from '@component-library/foundation/Themes/Themes.types';
+import { TextProps } from '@component-library/foundation/Text/Text.types';
 
 interface CardFields {
   fields: {
@@ -20,7 +24,12 @@ interface Fields {
 }
 
 type ContentCarouselProps = {
-  params: { [key: string]: string };
+  params: {
+    Theme: Theme; // TODO - this should reflect what CMS provides, not what FE consumes
+    HeadingTag: keyof JSX.IntrinsicElements; // TODO - this should reflect what CMS provides, not what FE consumes
+    HeadingSize: TextProps['variation']; // TODO - this should reflect what CMS provides, not what FE consumes
+    styles: string;
+  };
   fields: Fields;
 };
 
@@ -37,18 +46,26 @@ export const Default = (props: ContentCarouselProps): JSX.Element => {
     return <ContentCarouselDefaultComponent {...props} />;
   }
   return (
-    <div className={`component ${props.params.styles}`}>
-      <ul>
-        {props.fields.Cards.map((cards, index) => (
-          <li key={index}>
-            <Text field={cards.fields.Title} />
-            <br />
-            <RichText field={cards.fields.Text} />
-            <br />
-            <Image field={cards.fields.Image} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <CarouselContent
+        theme={props.params.Theme}
+        slides={props.fields.Cards.map((cards) => ({
+          title: (
+            <Text
+              tag={props.params.HeadingTag}
+              variation={props.params.HeadingSize}
+            >
+              <JssText tag={'span'} field={cards.fields.Title} />
+            </Text>
+          ),
+          body: (
+            <Text tag="p" variation="body-large">
+              <JssRichText tag={'span'} field={cards.fields.Text} />
+            </Text>
+          ),
+          image: <JssImage field={cards.fields.Image} />,
+        }))}
+      />
+    </>
   );
 };
