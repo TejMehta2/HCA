f1439559a3275a31bfc4242a247eae1aef492939
ashley-eqtr <146321241+ashley-eqtr@users.noreply.github.com>
2024-01-11 13:01:57 +0000

    feature/HWPD-1340/CardBlogBlock-implementation (#171)
    * feature/HWPD-1340/CardBlogBlock-implementation
    
    - Implemented CardBlogBlock component
    
    * feature/HWPD-1340/CardBlogBlock-implementation

---
+++ src/component-library/site-components/CardBlogBlock/CardBlogBlock.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { CardBlogBlockProps } from './CardBlogBlock.types';
+import styles from './CardBlogBlock.module.scss';
+import Themes from '../../foundation/Themes/Themes';
+
+const CardBlogBlock = (props: CardBlogBlockProps): JSX.Element => {
+  const { title, children, theme = 'H-HCA-Green-20', cta } = props;
+  return (
+    <Themes theme={theme}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.grid}>{children}</div>
+          <div className={styles.cta}>{cta}</div>
+        </div>
+      </div>
+    </Themes>
+  );
+};
+
+export default CardBlogBlock;
