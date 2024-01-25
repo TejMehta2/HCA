74d80cb660b22c70148ab8a23c37b56c854b4f38
naoumz <naoum.zilas@eqtr.com>
2024-01-25 12:27:04 +0000

    HWPD-1172
    Create a plain header component

---
+++ src/hcamain/src/components/PlainHeader/PlainHeader.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Field, RichText, Text } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Heading: Field<string>;
+  Title: Field<string>;
+  Text: Field<string>;
+}
+
+type PlainHeaderProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+const PlainHeaderDefaultComponent = (
+  props: PlainHeaderProps
+): JSX.Element => {
+  return (
+    <div className={`component ${props.params.styles}`}>
+      <div className="component-content">
+        <span className="is-empty-hint">Header with text</span>
+      </div>
+    </div>
+  );
+};
+
+export const Default = (props: PlainHeaderProps): JSX.Element => {
+  if (!props.fields) {
+    return <PlainHeaderDefaultComponent {...props} />;
+  }
+  return (
+    <div>
+      <Text field={props.fields.Heading} />
+      <br />
+      <Text field={props.fields.Title} />
+      <br />
+      <RichText field={props.fields.Text} />
+    </div>
+  );
+};
