8b92166d225226feeab83ada764e9d65cf0c57ea
Carmen Popovici <carmen.popovici@eqtr.com>
2024-01-25 15:24:32 +0200

    first commit for service line search and treatment search components

---
+++ src/hcamain/src/components/TreatmentsSearch/TreatmentsSearch.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Field,
+  Text as JssText,
+  RichText,
+  ComponentRendering,
+  Placeholder,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+type HCAIconFields = {
+  fields: {
+    SvgMarkup: Field<string>;
+  };
+};
+
+interface Fields {
+  TreatmentsSearchIcon: HCAIconFields;
+  TreatmentsSearchText: Field<string>;
+  Title: Field<string>;
+  Text: Field<string>;
+}
+
+type TreatmentsSearchProps = {
+  params: { [key: string]: string };
+  rendering: ComponentRendering;
+  fields: Fields;
+};
+
+const TreatmentsSearchDefaultComponent = (props: TreatmentsSearchProps): JSX.Element => (
+  <div className={`component ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">TreatmentsSearch no datasource</span>
+    </div>
+  </div>
+);
+
+export const Default = (props: TreatmentsSearchProps): JSX.Element => {
+  const phKey = `cta-buttons-${props.params.DynamicPlaceholderId}`;
+  if (!props.fields) {
+    return <TreatmentsSearchDefaultComponent {...props} />;
+  }
+  return (
+    <div className={`component ${props.params.styles}`}>
+      {props?.fields?.TreatmentsSearchIcon && (
+        <span
+          dangerouslySetInnerHTML={{
+            __html: props?.fields?.TreatmentsSearchIcon.fields.SvgMarkup.value,
+          }}
+        />
+      )}
+      <br />
+      <JssText field={props.fields.TreatmentsSearchText} />
+      <br />
+      <JssText field={props.fields.Title} />
+      <br />
+      <RichText tag="span" field={props.fields.Text} />
+      <br />
+      <Placeholder name={phKey} rendering={props.rendering} />
+    </div>
+  );
+};
