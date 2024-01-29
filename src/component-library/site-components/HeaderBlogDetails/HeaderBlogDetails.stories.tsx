97841109f2fce84ee2a62acdd5017fb591e0f25d
MichaelRendall <michael.rendall@eqtr.com>
2024-01-29 12:01:28 +0000

    HWPD-1580 blog article header

---
+++ src/component-library/site-components/HeaderBlogDetails/HeaderBlogDetails.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import HeaderBlogDetails from './HeaderBlogDetails';
+import type { Meta, StoryObj } from '@storybook/react';
+import Tags from '../../core-components/Tags/Tags';
+import Text from '../../foundation/Text/Text';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof HeaderBlogDetails> = {
+  title: 'site-components/HeaderBlogDetails',
+  component: HeaderBlogDetails,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: StoryObj<typeof HeaderBlogDetails> = {
+  args: {
+    theme: 'F-HCA-White',
+    tag: (
+      <Tags contentVariation="quote">
+        <a href="#">Announcement</a>
+      </Tags>
+    ),
+    date: <time dateTime="Sept 7, 2023">Sept 7, 2023</time>,
+    title: (
+      <Text tag="h1" variation="display-1">
+        The Harley Street Clinic retain CQC &apos;Outstanding&apos; rating
+      </Text>
+    ),
+  },
+};
