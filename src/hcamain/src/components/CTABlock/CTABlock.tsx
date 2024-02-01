3a1414ff4a2efc058ec1f891e347c48ee289a35e
naoumz <naoum.zilas@eqtr.com>
2024-02-01 15:14:03 +0000

    HWPD-1501 CTA Block
    CTA Block component

---
+++ src/hcamain/src/components/CTABlock/CTABlock.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Field, RichText, Placeholder, ComponentRendering, Text } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ButtonProps } from '@component-library/core-components/Button/Button.types';
+import { Theme, HeadingTag, HeadingSize } from 'src/types/params';
+
+interface Fields {
+  Heading: Field<string>;
+  Title: Field<string>;
+  Text: Field<string>;
+}
+
+type CTABlockProps = {
+  params: {
+    [key: string]: string;
+    Theme: Theme;
+    HeadingTag: HeadingTag;
+    HeadingSize: HeadingSize;
+  };
+
+  rendering: ComponentRendering;
+  fields: Fields;
+};
+
+const CTABlockDefaultComponent = (props: CTABlockProps): JSX.Element => {
+  return (
+    <div className={`component ${props.params.styles}`}>
+      <div className="component-content">
+        <span className="is-empty-hint">Header with text</span>
+      </div>
+    </div>
+  );
+};
+
+export const Default = (props: CTABlockProps): JSX.Element => {
+  const phKey = `cta-buttons-${props.params.DynamicPlaceholderId}`;
+  if (!props.fields) {
+    return <CTABlockDefaultComponent {...props} />;
+  }
+  const buttonSize: ButtonProps['size'] = 'large'; // Explicit type here to provide type safety
+  return (
+    <><div>
+      <Text field={props.fields.Heading} />
+      <br />
+      <Text field={props.fields.Title} />
+      <br />
+      <RichText field={props.fields.Text} />
+    </div><Placeholder name={phKey} rendering={props.rendering} size={buttonSize} /></>
+  );
+};
