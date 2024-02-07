59e7517693baa4ca71c53e7d109ce240db77b069
MichaelRendall <michael.rendall@eqtr.com>
2024-02-07 14:38:26 +0000

    HWPD-1464 added component to sitecore file

--- src/hcamain/src/components/LogoBlock/LogoBlock.tsx
+++ src/hcamain/src/components/LogoBlock/LogoBlock.tsx
@@ -5,13 +5,16 @@ import {
   ImageField,
   Placeholder,
   ComponentRendering,
-  Link as JssLink,
   RichText as JssRichText,
   Image,
-  Text,
+  Text as JSSText,
+  Link as JSSLink,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import { Theme, HeadingTag, HeadingSize } from 'src/types/params';
 import { ButtonProps } from '@component-library/core-components/Button/Button.types';
+import LogoBlock from '@component-library/site-components/LogoBlock/LogoBlock';
+import Text from 'temp/component-library/foundation/Text/Text';
+import AdvancedBlockHeader from '@component-library/components/AdvancedBlockHeader/AdvancedBlockHeader';
 
 type CTAIconFields = {
   fields: {
@@ -64,8 +67,50 @@ export const Default = (props: LogoBlockProps): JSX.Element => {
   }
   const buttonSize: ButtonProps['size'] = 'large'; // Explicit type here to provide type safety
 
+  console.log(props);
   return (
-    <div className={`component ${props.params.styles}`}>
+    <LogoBlock
+      theme={props.params.Theme}
+      header={
+        <AdvancedBlockHeader
+          subtitle={
+            <Text variation={'subheading-1'}>
+              <JSSText field={props.fields.Heading} />
+            </Text>
+          }
+          title={
+            <Text
+              tag={props.params.HeadingTag}
+              variation={props.params.HeadingSize}
+            >
+              <JSSText field={props.fields.Title} />
+            </Text>
+          }
+          body={
+            <Text tag="div" variation={'body-large'}>
+              <JssRichText
+                tag="p"
+                className="promo-text"
+                field={props.fields.Text}
+              />
+            </Text>
+          }
+          ctas={
+            <Placeholder
+              name={phKey}
+              rendering={props.rendering}
+              size={buttonSize}
+            />
+          }
+        />
+      }
+      logos={props.fields.Logos.map((logo, index) => (
+        <JSSLink key={index} field={logo.fields.Link}>
+          <Image field={logo.fields?.LogoImage} />
+        </JSSLink>
+      ))}
+    />
+    /* <div className={`component ${props.params.styles}`}>
       <Text field={props.fields.Heading} />
       <br />
       <Text field={props.fields.Title} />
@@ -85,6 +130,6 @@ export const Default = (props: LogoBlockProps): JSX.Element => {
         ))}
       </ul>
       <Placeholder name={phKey} rendering={props.rendering} size={buttonSize} />
-    </div>
+    </div> */
   );
 };
