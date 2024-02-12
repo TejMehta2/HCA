33cc4cea9175cf30ae7ee5b3f421e50cab299e7c
MichaelRendall <michael.rendall@eqtr.com>
2024-02-12 11:01:46 +0000

    HWPD-1503 cta block

--- src/component-library/site-components/CTABlock/CTABlock.tsx
+++ src/component-library/site-components/CTABlock/CTABlock.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { CTABlockProps } from './CTABlock.types';
 import styles from './CTABlock.module.scss';
 import Themes from '../../foundation/Themes/Themes';
-import Button from '../../core-components/Button/Button';
-import TextButton from '../../core-components/TextButton/TextButton';
 
 const CTABlock = (props: CTABlockProps): JSX.Element => {
   const { header, subheader, children, ctas, theme } = props;
@@ -16,21 +14,7 @@ const CTABlock = (props: CTABlockProps): JSX.Element => {
             <div className={styles['header']}>{header}</div>
             <div className={styles['body-text']}>{children}</div>
           </div>
-          {ctas && (
-            <div className={styles['ctas']}>
-              {ctas.button1 && (
-                <Button size="large" theme="full">
-                  {ctas.button1}
-                </Button>
-              )}
-              {ctas.button2 && (
-                <Button size="large" theme="outline">
-                  {ctas.button2}
-                </Button>
-              )}
-              {ctas.button3 && <TextButton>{ctas.button3}</TextButton>}
-            </div>
-          )}
+          {ctas && <div className={styles['ctas']}>{ctas}</div>}
         </div>
       </div>
     </Themes>
--- src/component-library/site-components/CTABlock/CTABlock.types.ts
+++ src/component-library/site-components/CTABlock/CTABlock.types.ts
@@ -5,10 +5,6 @@ export interface CTABlockProps {
   header: ReactNode | JSX.Element;
   subheader: ReactNode | JSX.Element;
   children: ReactNode | JSX.Element;
-  ctas?: {
-    button1?: ReactNode | JSX.Element;
-    button2?: ReactNode | JSX.Element;
-    button3?: ReactNode | JSX.Element;
-  };
+  ctas?: ReactNode | JSX.Element;
   theme: Theme;
 }
