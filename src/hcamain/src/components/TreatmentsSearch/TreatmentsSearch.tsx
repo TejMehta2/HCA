8d810017ada08491b194296801aa8865885dcfa8
kristina_greig <kristina.greig@eqtr.com>
2024-02-13 15:46:00 +0000

    integrate treatments search page

--- src/hcamain/src/components/TreatmentsSearch/TreatmentsSearch.tsx
+++ src/hcamain/src/components/TreatmentsSearch/TreatmentsSearch.tsx
@@ -1,49 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Text as JssText, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
+import SearchWrapper from '@component-library/site-components/SearchWrapper/SearchWrapper';
+import HeaderPlain from '@component-library/site-components/HeaderPlain/HeaderPlain';
+import SearchBar from '@component-library/components/SearchBar/SearchBar';
+import Text from '@component-library/foundation/Text/Text';
+import Checkboxes from '@component-library/core-components/Checkboxes/Checkboxes';
+import Checkbox from '@component-library/core-components/Checkbox/Checkbox';
+import Filters from '@component-library/site-components/Filters/Filters';
+import Sorting from '@component-library/components/Sorting/Sorting';
+import CardContent from '@component-library/components/CardContent/CardContent';
+import Pagination from '@component-library/core-components/Pagination/Pagination';
+import CardGrid from '@component-library/site-components/CardGrid/CardGrid';
+import Image from 'next/image';
+
 import {
-  Field,
-  Text as JssText,
-  RichText,
-} from '@sitecore-jss/sitecore-jss-nextjs';
-import { useI18n } from 'next-localization';
-
-type HCAIconFields = {
-  fields: {
-    SvgMarkup: Field<string>;
-  };
-};
-
-type FilterOptionsFields = {
-  fields: {
-    Header: Field<string>;
-    Filters: SortOptionsFields[];
-  };
-};
-
-type SortOptionsFields = {
-  fields: {
-    Filter: Field<string>;
-  };
-};
-
-interface Fields {
-  Heading: Field<string>;
-  Title: Field<string>;
-  Text: Field<string>;
-  SearchPlaceholder: Field<string>;
-  FilterOptionsIcon: HCAIconFields;
-  FilterOptionsText: Field<string>;
-  FilterOptions: FilterOptionsFields[];
-  SortOptionsIcon: HCAIconFields;
-  SortOptionsText: Field<string>;
-  SortOptions: SortOptionsFields[];
-  SearchResultsText: Field<string>;
-  ResultsPerPage: Field<string>;
-}
-
-type TreatmentsSearchProps = {
-  params: { [key: string]: string };
-  fields: Fields;
-};
+  TreatmentsResponse,
+  TreatmentsSearchProps,
+} from './TreatmentsSearch.types';
+import useSearchForm from 'src/hooks/useSearchForm/useSearchForm';
 
 const TreatmentsSearchDefaultComponent = (
   props: TreatmentsSearchProps
@@ -56,72 +30,242 @@ const TreatmentsSearchDefaultComponent = (
 );
 
 export const Default = (props: TreatmentsSearchProps): JSX.Element => {
-  const { t } = useI18n();
-  if (!props.fields) {
+  // Hooks
+  // const { t: localise } = useI18n();
+  const paginationRef = useRef<HTMLInputElement>(null);
+  const searchWrapperRef = useRef<HTMLDivElement>(null);
+
+  const { data, error, formHandlers, searchParams } =
+    useSearchForm<TreatmentsResponse>({
+      baseUrl: process.env.NEXT_PUBLIC_TREATMENTS_REQUEST_URL as string,
+    });
+
+  if (!props.fields || error) {
     return <TreatmentsSearchDefaultComponent {...props} />;
   }
-  return (
-    <div className={`component ${props.params.styles}`}>
-      <JssText field={props.fields.Heading} />
-      <br />
-      <JssText field={props.fields.Title} />
-      <br />
-      <RichText tag="span" field={props.fields.Text} />
-      <br />
-      <JssText field={props.fields.SearchPlaceholder} />
-      <br />
-      {props?.fields?.FilterOptionsIcon && (
-        <span
-          dangerouslySetInnerHTML={{
-            __html: props?.fields?.FilterOptionsIcon.fields.SvgMarkup.value,
-          }}
-        />
-      )}
-      <JssText field={props.fields.FilterOptionsText} />
-      <br />
-      <ul>
-        {props.fields.FilterOptions.map((filterOptions, index) => (
-          <li key={index}>
-            <br />
-            <JssText field={filterOptions.fields.Header} />
-            <br />
-            <ul>
-              {filterOptions.fields.Filters.map((filter, index) => (
-                <li key={index}>
-                  <JssText field={filter.fields.Filter} />
-                  <br />
-                </li>
-              ))}
-            </ul>
-            <br />
-          </li>
-        ))}
-      </ul>
-      <br />
-      {props?.fields?.SortOptionsIcon && (
-        <span
-          dangerouslySetInnerHTML={{
-            __html: props?.fields?.SortOptionsIcon.fields.SvgMarkup.value,
-          }}
-        />
-      )}
-      <JssText field={props.fields.SortOptionsText} />
-      <br />
-      <ul>
-        {props.fields.SortOptions.map((sortOptions, index) => (
-          <li key={index}>
-            <JssText field={sortOptions.fields.Filter} />
-            <br />
-          </li>
-        ))}
-      </ul>
-      <br />
-      <JssText field={props.fields.SearchResultsText} />
-      <JssText field={props.fields.ResultsPerPage} />
-      <p>Text: {t('close')}</p>
-      <p>Text: {t('show-more')}</p>
-      <p>Text: {t('showing')}</p>
-      <p>Text: {t('clear-all')}</p>
-    </div>
+
+  console.log(props);
+
+  // TODO - get/compute these from API response when API implemented
+  const resultsPerPage = props.fields.ResultsPerPage.value || 12;
+  const resultsLength = data?.treatments?.length || 0;
+  const resultsRange = `1-${resultsPerPage}`;
+  const pageCount = 3;
+
+  // Flatten CMS params for mapping
+  const filterByList = props.fields.FilterBy.map(
+    (item) => item.fields.Filter.value
   );
+  const SearchByList = props.fields.SearchBy.map(
+    (item) => item.fields.Filter.value
+  );
+
+  return (
+    <>
+      <form {...formHandlers}>
+        {/* Map CMS params */}
+        {[...filterByList, ...SearchByList].map((param) => (
+          <input
+            key={param}
+            type={'hidden'}
+            name={param.split('=')[0]}
+            value={param.split('=')[0]}
+          />
+        ))}
+
+        <SearchWrapper
+          ref={searchWrapperRef}
+          header={
+            <HeaderPlain
+              subheading={
+                <Text variation={'subheading-1'}>
+                  <JssText field={props.fields.Heading} />
+                </Text>
+              }
+              heading={
+                <Text
+                  variation={props.params.headingSize || 'display-2'}
+                  tag={props.params.headingTag || 'h2'}
+                >
+                  <JssText field={props.fields.Title} />
+                </Text>
+              }
+              search={
+                <SearchBar
+                  defaultValue={searchParams.get('searchString') || undefined}
+                  name={'searchString'}
+                  placeholder={props.fields.SearchPlaceholder.value}
+                />
+              }
+              theme={props.params.theme}
+              filters={
+                <Filters
+                  buttonText={
+                    <JssText field={props.fields.FilterOptionsText} />
+                  }
+                  buttonIcon={
+                    props?.fields?.FilterOptionsIcon && (
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html:
+                            props?.fields?.FilterOptionsIcon.fields.SvgMarkup
+                              .value,
+                        }}
+                      />
+                    )
+                  }
+                  resultsCount={resultsLength}
+                  filters={props.fields.FilterOptions.map(
+                    (option, optionIndex) => ({
+                      contentVariation: 'filters',
+                      title: option.fields.Header.value,
+                      children: (
+                        <Checkboxes>
+                          {option.fields.Filters.map((filter, filterIndex) => {
+                            const queryParts =
+                              filter.fields.Filter.value.split('=');
+                            const queryKey = queryParts[0];
+                            const queryValue = queryParts[1];
+                            return (
+                              <Checkbox
+                                key={filterIndex}
+                                id={
+                                  filter.id ||
+                                  `filter-${optionIndex}-${filterIndex}`
+                                }
+                                value={queryValue}
+                                name={queryKey}
+                                label={filter.fields.DisplayName.value}
+                                defaultChecked={
+                                  searchParams
+                                    .getAll(queryKey)
+                                    .includes(queryValue) || undefined
+                                }
+                              />
+                            );
+                          })}
+                        </Checkboxes>
+                      ),
+                    })
+                  )}
+                />
+              }
+              sort={
+                <Sorting
+                  buttonText={<JssText field={props.fields.SortOptionsText} />}
+                  buttonIcon={
+                    props?.fields?.SortOptionsIcon && (
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html:
+                            props?.fields?.SortOptionsIcon.fields.SvgMarkup
+                              .value,
+                        }}
+                      />
+                    )
+                  }
+                  options={props.fields.SortOptions.map((option, index) => {
+                    const queryParts = option.fields.Filter.value.split('=');
+                    const queryKey = queryParts[0];
+                    const queryValue = queryParts[1];
+                    return {
+                      id: option.id || `sort-by-${index}`,
+                      value: queryValue,
+                      labelText: option.fields.DisplayName.value,
+                      name: queryKey,
+                      defaultChecked: searchParams.get(queryKey) === queryValue,
+                    };
+                  })}
+                />
+              }
+            >
+              <Text variation="body-large" tag="div">
+                <RichText tag="div" field={props.fields.Text} />
+              </Text>
+            </HeaderPlain>
+          }
+          searchDetail={
+            <Text tag="h3" variation="heading-1">
+              <span>
+                <span>{resultsLength} articles including ‘</span>
+                <JssText field={props.fields.SearchResultsText} />
+                <span>’</span>{' '}
+              </span>
+            </Text>
+          }
+          showing={
+            <Text variation="body-medium">
+              <span>Showing {resultsRange}</span>
+            </Text>
+          }
+        >
+          <CardGrid theme={props.params.theme}>
+            {data?.treatments?.map((item, index) => (
+              <CardContent
+                key={index}
+                title={
+                  <Text variation="heading-1" tag="h4">
+                    {item.Title}
+                  </Text>
+                }
+                bodyCopy={
+                  <Text variation="body-large">{item.Description}</Text>
+                }
+                image={
+                  <Image src={item.Image} alt="" width="363" height="243" />
+                }
+                // TODO update 'learn more' link
+                link={
+                  <a href="#">
+                    <span>Learn more</span>
+                  </a>
+                }
+              />
+            ))}
+          </CardGrid>
+          <>
+            {pageCount > 1 && (
+              <Pagination
+                pageCount={pageCount}
+                currentPage={Number(searchParams.get('page')) || 1}
+                callback={(value) => {
+                  // Convert state-based pagination callback to native HTML input change event
+                  const pageField = paginationRef.current as HTMLInputElement;
+                  if (pageField) {
+                    const prevValue = pageField.value;
+                    pageField.stepUp(value - Number(pageField.value));
+                    const newValue = pageField.value;
+                    if (prevValue !== newValue) {
+                      const event = new Event('change', { bubbles: true });
+                      pageField.dispatchEvent(event);
+                      searchWrapperRef?.current?.scrollIntoView({
+                        behavior: 'smooth',
+                      });
+                    }
+                  }
+                }}
+              />
+            )}
+          </>
+          <input
+            type={'number'}
+            className="sr-only"
+            name="page"
+            defaultValue={Number(searchParams.get('page'))}
+            ref={paginationRef}
+            max={pageCount}
+            min={1}
+            aria-label="page"
+          />
+        </SearchWrapper>
+      </form>
+    </>
+  );
+
+  /* TODO - make localisation fields into props */
+
+  /* <p>Text: {localise('close')}</p>
+      <p>Text: {localise('show-more')}</p>
+      <p>Text: {localise('showing')}</p>
+      <p>Text: {localise('clear-all')}</p> */
 };
---
+++ src/hcamain/src/components/TreatmentsSearch/TreatmentsSearch.types.ts
@@ -0,0 +1,64 @@
+import { Field, Item } from '@sitecore-jss/sitecore-jss-nextjs';
+import { HeadingSize, HeadingTag, Theme } from 'src/types/params';
+
+export interface TreatmentsResponse {
+  treatments: {
+    Description: string;
+    Image: string;
+    Title: string;
+  }[];
+}
+
+export type HCAIconFields = {
+  fields: {
+    SvgMarkup: Field<string>;
+  };
+};
+
+export type FilterOptionFields = Item & {
+  fields: {
+    Header: Field<string>;
+    Filter: Field<string>;
+  };
+};
+
+export type FilterOptionsFields = Item & {
+  fields: {
+    Header: Field<string>;
+    Filters: SortOptionsFields[];
+  };
+};
+
+export type SortOptionsFields = Item & {
+  fields: {
+    DisplayName: Field<string>;
+    Filter: Field<string>;
+  };
+};
+
+export interface Fields {
+  Heading: Field<string>;
+  Title: Field<string>;
+  Text: Field<string>;
+  SearchPlaceholder: Field<string>;
+  FilterOptionsIcon: HCAIconFields;
+  FilterOptionsText: Field<string>;
+  FilterOptions: FilterOptionsFields[];
+  SortOptionsIcon: HCAIconFields;
+  SortOptionsText: Field<string>;
+  SortOptions: SortOptionsFields[];
+  SearchResultsText: Field<string>;
+  ResultsPerPage: Field<number>;
+  SearchBy: SortOptionsFields[];
+  FilterBy: FilterOptionFields[];
+}
+
+export type TreatmentsSearchProps = {
+  params: {
+    headingSize: HeadingSize;
+    headingTag: HeadingTag;
+    theme: Theme;
+    [key: string]: string;
+  };
+  fields: Fields;
+};
