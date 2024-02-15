260833d81787c3c8de16b4a71c1151e013349096
kristina_greig <kristina.greig@eqtr.com>
2024-02-15 11:13:59 +0000

    start talk to us integration

--- src/hcamain/src/components/TalkToUs/TalkToUs.tsx
+++ src/hcamain/src/components/TalkToUs/TalkToUs.tsx
@@ -4,8 +4,13 @@ import {
   Text as JssText,
   Image as JssImage,
   ImageField,
-  RichText,
+  RichText as JssRichText,
 } from '@sitecore-jss/sitecore-jss-nextjs';
+import ImageAndTextBlock from '@component-library/site-components/ImageAndTextBlock/ImageAndTextBlock';
+import Text from '@component-library/foundation/Text/Text';
+import ContactList from '@component-library/components/ContactList/ContactList';
+import Icons from '@component-library/foundation/Icons/Icons';
+import { Theme, HeadingSize, HeadingTag } from 'src/types/params';
 
 interface TelephoneNumberFields {
   phoneNumberLabel: { value: string };
@@ -57,92 +62,173 @@ interface Fields {
   };
 }
 
-type CallUsTodayCTAProps = {
-  params: { [key: string]: string };
+type TalkToUsProps = {
+  params: {
+    [key: string]: string;
+    Theme: Theme;
+    HeadingTag: HeadingTag;
+    HeadingSize: HeadingSize;
+  };
   fields: Fields;
 };
 
-const CallUsTodayCTADefaultComponent = (
-  props: CallUsTodayCTAProps
-): JSX.Element => {
-  return (
-    <div className={`component ${props.params.styles}`}>
-      <div className="component-content">
-        <span className="is-empty-hint">TalkToUs no datasource</span>
-      </div>
+const TalkToUsDefaultComponent = (props: TalkToUsProps): JSX.Element => (
+  <div className={`component promo ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">ImageShortText no datasource</span>
     </div>
-  );
-};
+  </div>
+);
 
-export const Default = (props: CallUsTodayCTAProps): JSX.Element => {
+export const Default = (props: TalkToUsProps): JSX.Element => {
   if (!props.fields) {
-    return <CallUsTodayCTADefaultComponent {...props} />;
+    return <TalkToUsDefaultComponent {...props} />;
   }
+  console.log(props);
+
+  const contactListItems = [];
+
+  props.fields.data.item.contactUnits.contactUnitList.map((contactUnit) => {
+    const numbers = contactUnit.telephoneNumber.telephoneNumberList.map(
+      (telephoneNumber, index) => {
+        return (
+          <Text tag="p" variation="display-6" key={index}>
+            <JssText field={telephoneNumber.phoneNumber} />
+          </Text>
+        );
+        //                 <li key={index}>
+        //                   <JssText field={telephoneNumber.phoneNumberLabel} />
+        //                   <br />
+        //                   <JssText field={telephoneNumber.phoneNumber} />
+        //                   <br />
+        //                   <JssText field={telephoneNumber.internationPhoneNumber} />
+        //                 </li>
+        //               )
+        //             )}
+      }
+    );
+
+    const contactListItem = {
+      title: (
+        <Text tag="h4" variation="subheading-2">
+          <JssText field={contactUnit.contactUnitName} />
+        </Text>
+      ),
+      icon: <Icons iconName="iconClock"></Icons>,
+      openingHours: (
+        <Text tag="p" variation="body-large">
+          Monday to Friday 8am - 6pm
+        </Text>
+      ),
+      number: numbers,
+    };
+    contactListItems.push(contactListItem);
+  });
+
   return (
-    <div className={`component ${props.params.styles}`}>
-      <JssText field={props.fields.data.item.heading.jsonValue} />
-      <br />
-      <JssText field={props.fields.data.item.title.jsonValue} />
-      <br />
-      <RichText field={props.fields.data.item.text.jsonValue} />
-      <br />
-      <JssImage field={props.fields.data.item.image.jsonValue} />
-      <br />
-      <ul>
-        {props.fields.data.item.contactUnits.contactUnitList.map(
-          (contactUnit, index) => (
-            <li key={index}>
-              <JssText field={contactUnit.contactUnitName} />
-              <br />
-              <ul>
-                {contactUnit.telephoneNumber.telephoneNumberList.map(
-                  (telephoneNumber, index) => (
-                    <li key={index}>
-                      <JssText field={telephoneNumber.phoneNumberLabel} />
-                      <br />
-                      <JssText field={telephoneNumber.phoneNumber} />
-                      <br />
-                      <JssText field={telephoneNumber.internationPhoneNumber} />
-                    </li>
-                  )
-                )}
-              </ul>
-              <br />
-              <span>Opening Hours</span>
-              <br />
-              <ul>
-                {contactUnit.children.results.map((children, index) => (
-                  <li key={index}>
-                    <ul>
-                      {children.children.results.map((openingHours, index) => (
-                        <li key={index}>
-                          <ul>
-                            {openingHours.dayOfWeek.dayOfWeekList.map(
-                              (day, index) => (
-                                <li key={index}>
-                                  <JssText field={day.dayName} />
-                                </li>
-                              )
-                            )}
-                          </ul>
-                          <br />
-                          <JssText field={openingHours.opens} />
-                          <br />
-                          <JssText field={openingHours.closes} />
-                          <br />
-                          <JssText field={openingHours.validFrom} />
-                          <br />
-                          <JssText field={openingHours.validThrough} />
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          )
-        )}
-      </ul>
-    </div>
+    <ImageAndTextBlock
+      theme={props.params.Theme || 'A-HCA-Main-Turquoise'}
+      imageAlignment="left"
+      length="short"
+      header={
+        <Text
+          tag={props.params.HeadingTag || 'h4'}
+          variation={props.params.HeadingSize || 'subheading-2'}
+        >
+          <JssText field={props.fields.data.item.title.jsonValue} />
+        </Text>
+      }
+      image={<JssImage field={props.fields.data.item.image.jsonValue} />}
+    >
+      <ContactList
+        items={
+          contactListItems
+          // {
+          //   title: (
+          //     <Text tag="h4" variation="subheading-2">
+          //       Embassy team
+          //     </Text>
+          //   ),
+          //   number: (
+          //     <Text tag="p" variation="display-6">
+          //       020 3131 5978
+          //     </Text>
+          //   ),
+          //   icon: <Icons iconName="iconClock"></Icons>,
+          //   openingHours: (
+          //     <Text tag="p" variation="body-large">
+          //       Monday to Friday 8am - 6pm
+          //     </Text>
+          //   ),
+          // },
+        }
+      />
+    </ImageAndTextBlock>
+
+    // <div className={`component ${props.params.styles}`}>
+    //   <JssText field={props.fields.data.item.heading.jsonValue} />
+    //   <br />
+    //   <JssText field={props.fields.data.item.title.jsonValue} />
+    //   <br />
+    //   <JssRichText field={props.fields.data.item.text.jsonValue} />
+    //   <br />
+    //   <JssImage field={props.fields.data.item.image.jsonValue} />
+    //   <br />
+    //   <ul>
+    //     {props.fields.data.item.contactUnits.contactUnitList.map(
+    //       (contactUnit, index) => (
+    //         <li key={index}>
+    //           <JssText field={contactUnit.contactUnitName} />
+    //           <br />
+    //           <ul>
+    //             {contactUnit.telephoneNumber.telephoneNumberList.map(
+    //               (telephoneNumber, index) => (
+    //                 <li key={index}>
+    //                   <JssText field={telephoneNumber.phoneNumberLabel} />
+    //                   <br />
+    //                   <JssText field={telephoneNumber.phoneNumber} />
+    //                   <br />
+    //                   <JssText field={telephoneNumber.internationPhoneNumber} />
+    //                 </li>
+    //               )
+    //             )}
+    //           </ul>
+    //           <br />
+    //           <span>Opening Hours</span>
+    //           <br />
+    //           <ul>
+    //             {contactUnit.children.results.map((children, index) => (
+    //               <li key={index}>
+    //                 <ul>
+    //                   {children.children.results.map((openingHours, index) => (
+    //                     <li key={index}>
+    //                       <ul>
+    //                         {openingHours.dayOfWeek.dayOfWeekList.map(
+    //                           (day, index) => (
+    //                             <li key={index}>
+    //                               <JssText field={day.dayName} />
+    //                             </li>
+    //                           )
+    //                         )}
+    //                       </ul>
+    //                       <br />
+    //                       <JssText field={openingHours.opens} />
+    //                       <br />
+    //                       <JssText field={openingHours.closes} />
+    //                       <br />
+    //                       <JssText field={openingHours.validFrom} />
+    //                       <br />
+    //                       <JssText field={openingHours.validThrough} />
+    //                     </li>
+    //                   ))}
+    //                 </ul>
+    //               </li>
+    //             ))}
+    //           </ul>
+    //         </li>
+    //       )
+    //     )}
+    //   </ul>
+    // </div>
   );
 };
