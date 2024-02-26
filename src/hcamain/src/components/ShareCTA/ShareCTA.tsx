e0407b289093222f796f62820662959ea14d6a73
kristina_greig <kristina.greig@eqtr.com>
2024-02-26 12:02:42 +0000

    update theme in share cta to new theme

--- src/hcamain/src/components/ShareCTA/ShareCTA.tsx
+++ src/hcamain/src/components/ShareCTA/ShareCTA.tsx
@@ -83,7 +83,7 @@ export const Default = (props: ShareCTAProps): JSX.Element => {
     text: props.fields.data.contextItem?.description?.value || '',
   };
   return (
-    <Themes theme={'J-HCA-Turquoise-10'}>
+    <Themes theme={'L-HCA-Teal-5'}>
       <ShareCTA
         shareCtaText={
           <JssRichText
@@ -112,7 +112,7 @@ export const Default = (props: ShareCTAProps): JSX.Element => {
             <JssText field={props.fields.data.item.text?.jsonValue} />
           </Text>
         }
-        theme="F-HCA-White"
+        theme="A-HCA-White"
       >
         <Button size="large" variation="square-outline">
           <button
