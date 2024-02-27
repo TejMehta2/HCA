443d267f4577b035d5e080f985fc8a5c843d4219
kristina_greig <kristina.greig@eqtr.com>
2024-02-27 16:55:34 +0000

    remove theme prop from marketing preferences and update background

--- src/component-library/site-components/MarketingPreferences/MarketingPreferences.tsx
+++ src/component-library/site-components/MarketingPreferences/MarketingPreferences.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { MarketingPreferencesProps } from './MarketingPreferences.types';
 import styles from './MarketingPreferences.module.scss';
-import Themes from '../../foundation/Themes/Themes';
 
 const MarketingPreferences = (
   props: MarketingPreferencesProps
 ): JSX.Element => {
-  const { title, bodyCopy, preferences, theme = 'K-HCA-Fern-20' } = props;
+  const { title, bodyCopy, preferences } = props;
   return (
-    <Themes theme={theme}>
-      <div className={styles.wrapper}>
-        {title && <div>{title}</div>}
-        {bodyCopy && <div className={styles.copy}>{bodyCopy}</div>}
-        {preferences && <div>{preferences}</div>}
-      </div>
-    </Themes>
+    <div className={styles.wrapper}>
+      {title && <div>{title}</div>}
+      {bodyCopy && <div className={styles.copy}>{bodyCopy}</div>}
+      {preferences && <div>{preferences}</div>}
+    </div>
   );
 };
 
