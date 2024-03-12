9f2d0992a5131d035a2cd4dd2a8a981b6c87ff73
MichaelRendall <michael.rendall@eqtr.com>
2024-03-12 10:12:07 +0000

    HWPD-2006 diamond line

--- src/component-library/site-components/DiamondLine/DiamondLine.types.ts
+++ src/component-library/site-components/DiamondLine/DiamondLine.types.ts
@@ -1,8 +1,6 @@
-import { ReactNode } from 'react';
 import { Theme } from '../../foundation/Themes/Themes.types';
 
 export interface DiamondLineProps {
-  children?: ReactNode | JSX.Element;
   side?: 'left' | 'right';
   theme?: Theme;
 }
