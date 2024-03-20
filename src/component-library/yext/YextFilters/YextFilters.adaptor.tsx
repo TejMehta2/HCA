124830008f92b88f7b87ad8c366cf72f069fef0a
kristina_greig <kristina.greig@eqtr.com>
2024-03-20 09:03:10 +0000

    get filtering results functioning

--- src/component-library/yext/YextFilters/YextFilters.adaptor.tsx
+++ src/component-library/yext/YextFilters/YextFilters.adaptor.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { useSearchState, useSearchActions } from '@yext/search-headless-react';
-import { executeSearch } from '@yext/search-ui-react';
 import YextFilters from './YextFilters';
+import {
+  Matcher,
+  NumberRangeValue,
+  useSearchActions,
+  useSearchState,
+} from '@yext/search-headless-react';
 
 import Checkbox from '../../core-components/Checkbox/Checkbox';
 import Checkboxes from '../../core-components/Checkboxes/Checkboxes';
@@ -17,50 +21,51 @@ const YextFiltersAdaptor = (): JSX.Element => {
   console.log(facets && facets[0].displayName);
 
   const resultCount = 20;
-  // const title = 'Tests, Scans, Treatments';
-  // const filters = [
-  //   {
-  //     id: '1',
-  //     label: 'Conditions (20)',
-  //     name: 'conditions',
-  //     value: 'conditions',
-  //   },
 
-  //   { id: '2', label: 'Tests (15)', name: 'tests', value: 'tests' },
-
-  //   { id: '3', label: 'Treatments (2)', name: 'tests', value: 'tests' },
-  // ];
+  const searchActions = useSearchActions();
 
   const handleFacetClick = (
     value: string | number | boolean | NumberRangeValue,
     selected: boolean,
+    fieldId,
     matcher = Matcher.Equals
   ) => {
     searchActions.setFacetOption(fieldId, { matcher, value }, selected);
     searchActions.executeVerticalQuery();
   };
 
-  return facets?.length && facets[0] ? (
-    <YextFilters
-      filtersTitle={facets[0]?.displayName}
-      resultsCount={resultCount}
-    >
-      <Checkboxes>
-        {facets[0]?.options.map((filter, index) => (
-          <Checkbox
-            id={filter.value}
-            label={filter.displayName}
-            name={facets[0]?.fieldId}
-            value={filter.value}
-            key={index}
-            onChange={() => handleFacetClick(filter.value, !o.selected)}
-          ></Checkbox>
-        ))}
-      </Checkboxes>
-    </YextFilters>
-  ) : (
-    <></>
-  );
+  const filters =
+    facets?.length &&
+    facets.map((facet) => {
+      return (
+        <YextFilters
+          filtersTitle={facet?.displayName}
+          resultsCount={resultCount}
+          key={facet.fieldId}
+        >
+          <Checkboxes>
+            {facet.options.map((filter, index) => (
+              <Checkbox
+                id={filter.value}
+                label={filter.displayName}
+                name={facet.fieldId}
+                value={filter.value}
+                key={index}
+                onChange={() =>
+                  handleFacetClick(
+                    filter.value,
+                    !filter.selected,
+                    facet.fieldId
+                  )
+                }
+              ></Checkbox>
+            ))}
+          </Checkboxes>
+        </YextFilters>
+      );
+    });
+
+  return facets && facets?.length ? filters : <></>;
 };
 
 export default YextFiltersAdaptor;
