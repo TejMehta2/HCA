923c2de0325b15e439b131fac67a4d42a1ee7b21
kristina_greig <kristina.greig@eqtr.com>
2024-03-24 22:39:30 +0000

    split out mapping cards depending on api or sitecore data

---
+++ src/hcamain/src/components/LocationCards/LocationCardDefault.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import CardMap from '@component-library/components/CardMap/CardMap';
+import Text from '@component-library/foundation/Text/Text';
+
+import {
+  Text as JssText,
+  Image as JssImage,
+  RichText as JssRichText,
+  Field,
+  ImageField,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+type Location = {
+  title?: { value?: string };
+  image?: { value?: ImageField };
+  city?: { value?: string };
+  street?: { value?: string };
+  postCode?: { value?: string };
+  getDirections?: { value?: string };
+  url: { path?: string };
+};
+
+type LocationCardDefault = {
+  locations: Location[];
+  directionsText: Field<string>;
+  linkText: Field<string>;
+};
+
+export const LocationCardDefault = (props: LocationCardDefault) => {
+  const { locations, directionsText, linkText } = props;
+
+  return locations.map((location: Location, index) => {
+    console.log(location);
+    return (
+      <CardMap
+        key={index}
+        title={
+          <Text variation="heading-1" tag="h4">
+            <JssText field={location.title} />
+          </Text>
+        }
+        address={
+          <>
+            {location?.street?.value && (
+              <Text variation={'body-large'} tag="span">
+                <JssText field={location?.street} />
+                &nbsp;
+              </Text>
+            )}
+            {location?.postCode?.value && (
+              <Text variation={'body-large'} tag="span">
+                <JssText field={location?.postCode} />
+                &nbsp;
+              </Text>
+            )}
+            {location?.city?.value && (
+              <Text variation={'body-large'} tag="span">
+                <JssText field={location?.city} />
+              </Text>
+            )}
+          </>
+        }
+        image={<JssImage field={location?.image?.value} />}
+        ctas={{
+          button1: (
+            <a href={location.url.path}>
+              <JssRichText field={linkText} />
+            </a>
+          ),
+          button2: (
+            <a href={location?.getDirections?.value}>
+              <span>
+                <JssText field={directionsText} />
+              </span>
+            </a>
+          ),
+        }}
+      />
+    );
+  });
+};
--- src/hcamain/src/components/LocationCards/LocationCards.tsx
+++ src/hcamain/src/components/LocationCards/LocationCards.tsx
@@ -1,33 +1,124 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import {
   Field,
   Item,
   ImageField,
   Text as JssText,
   Link as JssLink,
-  Image as JssImage,
   RichText as JssRichText,
   LinkField,
   GetStaticComponentProps,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import Params from 'src/types/params';
-import CardMap from '@component-library/components/CardMap/CardMap';
 import Text from '@component-library/foundation/Text/Text';
 import CarouselCards from '@component-library/site-components/CarouselCards/CarouselCards';
 import CardBlock from '@component-library/site-components/CardBlock/CardBlock';
 import AdvancedBlockHeader from '@component-library/components/AdvancedBlockHeader/AdvancedBlockHeader';
 
 import getBaselineParams from 'lib/getBaselineParams';
-import {
-  Autocomplete,
-  LocationsSearchProps,
-  SearchResponse,
-} from '../LocationsSearch/LocationsSearch.types';
-import unpackFilterOption from 'lib/unpackFilterOption';
 import useSearchForm from '@component-library/hooks/useSearchForm/useSearchForm';
-import { ApiResponse } from 'src/types/searchProps';
+import { ApiResponse, ApiSearchProps } from 'src/types/searchProps';
+import LocationCardApi from './LocationCardApi';
+import { LocationCardDefault } from './LocationCardDefault';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_DATALAYER_URL}/locations`;
+
+export interface SearchResponse {
+  meta: Meta;
+  response: Response;
+}
+
+export interface Meta {
+  uuid: string;
+  errors: unknown[];
+}
+
+export interface Response {
+  businessId: number;
+  queryId: string;
+  resultsCount: number;
+  results: Result[];
+  appliedQueryFilters: unknown[];
+  facets: unknown[];
+  source: string;
+  searchIntents: unknown[];
+  locationBias: null;
+}
+
+export interface Result {
+  data: Data;
+  highlightedFields: HighlightedFields;
+}
+
+export interface Data {
+  id: string;
+  type: string;
+  title: string;
+  description: string;
+  name: string;
+  imageUrl: null;
+  url: string;
+  uid: number;
+}
+
+export interface HighlightedFields {
+  name: null;
+  description: null;
+  title: null;
+}
+
+export interface Autocomplete {
+  meta: Meta;
+  response: AutocompleteResponse;
+}
+
+export interface AutocompleteResponseMeta {
+  uuid: string;
+  errors: unknown[];
+}
+
+export interface AutocompleteResponse {
+  input: AutocompleteResponseInput;
+  results: AutocompleteResponseResult[];
+}
+
+export interface AutocompleteResponseInput {
+  value: string;
+  queryIntents: unknown[];
+}
+
+export interface AutocompleteResponseResult {
+  value: string;
+  matchedSubstrings: unknown[];
+  queryIntents: unknown[];
+  verticalKeys: unknown[];
+}
+
+export type HCAIconFields = {
+  fields?: {
+    SvgMarkup?: Field<string>;
+  };
+};
+
+export interface FilterOption {
+  displayName: string;
+  fields: {
+    DisplayName?: Field<string>;
+    Filter?: Field<string>; // e.g. { value: 'locationId' }
+    FilterValueGuid?: {
+      id: string; //  e.g. { id: 'Birmingham' }
+    };
+    FilterValueString: Field<string>; // e.g. { value: 'Birmingham' }
+  };
+}
+
+export interface FilterCategory {
+  displayName: string;
+  fields: {
+    Header: Field<string>;
+    Filters: FilterOption[];
+  };
+}
 
 type CTAIconFields = {
   svgMarkup?: Field<string>;
@@ -61,7 +152,7 @@ interface Fields {
       };
       cTALink?: { jsonValue?: LinkField };
       locations?: {
-        pagesList?: LocationsFields[];
+        PagesList?: LocationsFields[];
       };
       filterOptions?: {
         filterOptionsList?: FilterOptionFields[];
@@ -77,6 +168,25 @@ interface Fields {
       conditionId?: string;
     };
   };
+  Heading?: Field<string>;
+  Title?: Field<string>;
+  Text?: Field<string>;
+  SearchPlaceholder?: Field<string>;
+  FilterOptionsIcon?: HCAIconFields;
+  FilterOptionsText?: Field<string>;
+  FilterOptions?: FilterCategory[];
+  SortOptionsIcon?: HCAIconFields;
+  SortOptionsText?: Field<string>;
+  SortOptions?: FilterOption[];
+  SearchResultsText?: Field<string>;
+  ResultsPerPage?: Field<number>;
+  SearchBy?: FilterOption[];
+  FilterBy?: FilterOption[];
+
+  GridViewIcon?: HCAIconFields;
+  GridViewText?: Field<string>;
+  MapViewIcon?: HCAIconFields;
+  MapViewText?: Field<string>;
 }
 
 type LocationCardsProps = {
@@ -95,166 +205,18 @@ const LocationCardsDefaultComponent = (
   </div>
 );
 
-const MapCards = (props: LocationCardsProps, data) => {
-  // const isApiData = props.fields?.data?.item?.locations?.PagesList &&
-  // props.fields?.data?.item?.locations?.PagesList.length
-
-  const isApiData = true;
-
-  const locations = isApiData
-    ? data
-    : props.fields?.data?.item?.locations?.PagesList;
-
-  console.log(locations);
-  return (
-    locations &&
-    locations.map((location, index) => {
-      return (
-        <CardMap
-          key={index}
-          title={
-            <Text variation="heading-1" tag="h4">
-              {isApiData ? (
-                location.data.title
-              ) : (
-                <JssText field={location?.data.title} />
-              )}
-            </Text>
-          }
-          address={
-            <>
-              {location?.city?.value && (
-                <Text variation={'body-large'} tag="span">
-                  <JssText field={location?.city} />
-                  &nbsp;
-                </Text>
-              )}
-              {location?.street?.value && (
-                <Text variation={'body-large'} tag="span">
-                  <JssText field={location?.street} />
-                  &nbsp;
-                </Text>
-              )}
-              {location?.postCode?.value && (
-                <Text variation={'body-large'} tag="span">
-                  <JssText field={location?.postCode} />
-                </Text>
-              )}
-            </>
-          }
-          image={
-            isApiData ? (
-              <img src={location.data.imageUrl} alt={location?.data.title} />
-            ) : (
-              <JssImage field={location?.image?.jsonValue} />
-            )
-          }
-          ctas={{
-            button1: (
-              <a href={isApiData ? location?.url?.path : location.url}>
-                <JssRichText
-                  field={props?.fields?.data?.item?.cTAText?.jsonValue}
-                />
-              </a>
-            ),
-            button2: (
-              <a
-                href={
-                  isApiData
-                    ? location.directions
-                    : location?.getDirections?.value
-                }
-              >
-                <span>
-                  <JssText
-                    field={
-                      props?.fields?.data?.item?.getDirectionsText?.jsonValue
-                    }
-                  />
-                </span>
-              </a>
-            ),
-          }}
-        />
-      );
-    })
-  );
-};
-
-// const mapApiCards = (data, props) => {
-//   console.log(data);
-//   return data?.map((location, index) => (
-//     <CardMap
-//       key={index}
-//       title={
-//         <Text variation="heading-1" tag="h4">
-//           {location?.title}
-//         </Text>
-//       }
-//       address={
-//         <>
-//           {location?.city?.value && (
-//             <Text variation={'body-large'} tag="span">
-//               <JssText field={location?.city} />
-//               &nbsp;
-//             </Text>
-//           )}
-//           {location?.street?.value && (
-//             <Text variation={'body-large'} tag="span">
-//               <JssText field={location?.street} />
-//               &nbsp;
-//             </Text>
-//           )}
-//           {location?.postCode?.value && (
-//             <Text variation={'body-large'} tag="span">
-//               <JssText field={location?.postCode} />
-//             </Text>
-//           )}
-//         </>
-//       }
-//       image={<JssImage field={location?.image?.jsonValue} />}
-//       ctas={{
-//         button1: (
-//           <a href={location?.url}>
-//             {' '}
-//             <JssRichText
-//               field={props?.fields?.data?.item?.cTAText?.jsonValue}
-//             />
-//           </a>
-//         ),
-//         button2: (
-//           <a href={location?.directions}>
-//             <JssText
-//               field={props?.fields?.data?.item?.getDirectionsText?.jsonValue}
-//             />
-//           </a>
-//         ),
-//       }}
-//     />
-//   ));
-// };
-
 export const Grid = (props: LocationCardsProps): JSX.Element => {
-  if (!props.fields) {
-    return <LocationCardsDefaultComponent {...props} />;
-  }
-
-  const { fallbackData, fields, params } = props;
+  const { fallbackData, fields } = props;
 
   // Set up default baseline parameters from CMS
-  const { defaultLimit, defaultOffset, baselineParams } =
-    getBaselineParams(props);
+  const { baselineParams } = getBaselineParams(props);
 
   // Hooks
-  const searchWrapperRef = useRef<HTMLDivElement>(null);
-  const {
-    data,
-    error,
-    formHandlers,
-    searchParams,
-    autocompleteData,
-    autocompleteError,
-  } = useSearchForm<SearchResponse, Autocomplete>({
+
+  const { data, error, autocompleteError } = useSearchForm<
+    SearchResponse,
+    Autocomplete
+  >({
     baseUrl: BASE_URL,
     baselineParams: [...baselineParams, ['verticalKey', 'locations']],
     fallbackData: fallbackData,
@@ -264,37 +226,10 @@ export const Grid = (props: LocationCardsProps): JSX.Element => {
     return <LocationCardsDefaultComponent {...props} />;
   }
 
-  // Mutable query-based params
-  const limit = Number(searchParams.get('limit')) || defaultLimit;
-  const offset = Number(searchParams.get('offset')) || defaultOffset;
+  // const isApiData = props.fields?.data?.item?.locations?.PagesList &&
+  // props.fields?.data?.item?.locations?.PagesList.length
 
-  // Computed properties
-  const resultsCount = data?.response.resultsCount || 0;
-  const rangeStart = offset + 1;
-  const rangeEnd = Math.min(offset + limit, resultsCount);
-  const resultsRange = `${rangeStart}-${rangeEnd}`;
-
-  // Parse filter options to be used in multiple components
-  const filterCategories = props.fields?.FilterOptions?.map((category) => ({
-    title: category?.displayName || '',
-    fields: category.fields?.Filters?.map((option) => {
-      const { id, key, value, displayName } = unpackFilterOption(option);
-      return {
-        id,
-        value,
-        name: key,
-        label: displayName,
-        defaultChecked: searchParams.getAll(key).includes(value),
-      };
-    }),
-  }));
-
-  const activeFilters = filterCategories?.reduce((previous, { fields }) => {
-    return [
-      ...previous,
-      ...fields.filter(({ defaultChecked }) => defaultChecked),
-    ];
-  }, []);
+  const isApiData = true;
 
   return (
     <CardBlock
@@ -312,7 +247,7 @@ export const Grid = (props: LocationCardsProps): JSX.Element => {
           }
           body={
             <Text variation={'body-large'}>
-              <JssText field={props.fields.data?.item?.text?.jsonValue} />
+              <JssText field={props.fields?.data?.item?.text?.jsonValue} />
             </Text>
           }
           paddingSize="small"
@@ -332,29 +267,52 @@ export const Grid = (props: LocationCardsProps): JSX.Element => {
         )
       }
     >
-      {/* {props.fields?.data?.item?.locations?.PagesList &&
-      props.fields?.data?.item?.locations?.PagesList.length */}
-      {MapCards(props, data?.response.results)}
-      {/* mapApiCards(data?.response.results, props)
-        : mapApiCards(data?.response.results, props)} */}
-
-      {/* {data?.response.results?.map((item) => {
-          console.log(item);
-          const { data } = item;
-          const { id, title, name, description, imageUrl, url, directions } =
-            data;
-          return <Text>{title}</Text>;
-        })} */}
-
-      {/* {MapCards(props)} */}
+      {isApiData ? (
+        <LocationCardApi
+          locations={data?.response.results}
+          linkText={props?.fields?.data?.item?.cTAText?.jsonValue}
+          directionsText={
+            props?.fields?.data?.item?.getDirectionsText?.jsonValue
+          }
+        />
+      ) : (
+        <LocationCardDefault
+          locations={props.fields?.data?.item?.locations?.PagesList}
+          linkText={props?.fields?.data?.item?.cTAText?.jsonValue}
+          directionsText={
+            props?.fields?.data?.item?.getDirectionsText?.jsonValue
+          }
+        />
+      )}
     </CardBlock>
   );
 };
 
 export const Slider = (props: LocationCardsProps): JSX.Element => {
-  if (!props.fields) {
+  const { fallbackData, fields } = props;
+
+  // Set up default baseline parameters from CMS
+  const { baselineParams } = getBaselineParams(props);
+
+  // Hooks
+
+  const { data, error, autocompleteError } = useSearchForm<
+    SearchResponse,
+    Autocomplete
+  >({
+    baseUrl: BASE_URL,
+    baselineParams: [...baselineParams, ['verticalKey', 'locations']],
+    fallbackData: fallbackData,
+  });
+
+  if (!fields || error || autocompleteError) {
     return <LocationCardsDefaultComponent {...props} />;
   }
+
+  // const isApiData = props.fields?.data?.item?.locations?.PagesList &&
+  // props.fields?.data?.item?.locations?.PagesList.length
+
+  const isApiData = true;
 
   return (
     <CarouselCards
@@ -369,7 +327,7 @@ export const Slider = (props: LocationCardsProps): JSX.Element => {
       }
       bodyCopy={
         <Text variation={'body-large'}>
-          <JssText field={props.fields.data?.item?.text?.jsonValue} />
+          <JssText field={props.fields?.data?.item?.text?.jsonValue} />
         </Text>
       }
       link={
@@ -385,7 +343,23 @@ export const Slider = (props: LocationCardsProps): JSX.Element => {
         )
       }
     >
-      {MapCards(props)}
+      {/* {isApiData ? (
+        <LocationCardApi
+          locations={data?.response.results}
+          linkText={props?.fields?.data?.item?.cTAText?.jsonValue}
+          directionsText={
+            props?.fields?.data?.item?.getDirectionsText?.jsonValue
+          }
+        />
+      ) : (
+        <LocationCardDefault
+          locations={props.fields?.data?.item?.locations?.PagesList}
+          linkText={props?.fields?.data?.item?.cTAText?.jsonValue}
+          directionsText={
+            props?.fields?.data?.item?.getDirectionsText?.jsonValue
+          }
+        />
+      )} */}
     </CarouselCards>
   );
 };
