ff1e5d5dd13800658c9b4f61d45918cf6d5f86fd
Carmen Popovici <carmen.popovici@eqtr.com>
2024-04-02 10:59:43 +0300

    prettier

--- src/hcamain/src/Layout.tsx
+++ src/hcamain/src/Layout.tsx
@@ -69,12 +69,11 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
         {headLinks.map((headLink) => (
           <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
         ))}
-       
       </Head>
 
       {/* root placeholder for the app, which we add components to using route data */}
       <div className={mainClassPageEditing}>
-      {route && (
+        {route && (
           <Placeholder
             name="headless-head"
             rendering={route}
