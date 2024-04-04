b8c923efdbce818f18e4dfb839ac7aee0d9c477c
kristina_greig <kristina.greig@eqtr.com>
2024-04-04 15:19:46 +0100

    update types

--- src/component-library/core-components/Checkbox/Checkbox.types.ts
+++ src/component-library/core-components/Checkbox/Checkbox.types.ts
@@ -1,10 +1,12 @@
 import { ChangeEventHandler } from 'react';
 
+import { NumberRangeValue } from '@yext/search-headless-react';
+
 export interface CheckboxProps {
   label: string | JSX.Element;
   name: string;
-  id: string;
-  value: string | number;
+  id: string | number | boolean | NumberRangeValue;
+  value: string | number | boolean | NumberRangeValue;
   mode?: 'light' | 'dark' | 'medium';
   disabled?: boolean;
   indeterminate?: boolean;
