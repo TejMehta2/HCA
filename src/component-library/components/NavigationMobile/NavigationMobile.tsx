97329a018ab173b311c0bd4c97023e0f40ac15a5
kristina_greig <kristina.greig@eqtr.com>
2024-04-30 11:47:05 +0100

    amends to filters to page content behind can't be scrolled and spacing below the buttons

--- src/component-library/components/NavigationMobile/NavigationMobile.tsx
+++ src/component-library/components/NavigationMobile/NavigationMobile.tsx
@@ -50,10 +50,10 @@ const NavigationMobile = (props: NavigationProps): JSX.Element => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = '';
     }
     return () => {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = '';
     };
   }, [isOpen]);
 
