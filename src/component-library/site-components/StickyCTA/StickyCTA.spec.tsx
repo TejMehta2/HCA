48818c110c32e82befe3cf790df52dcc21e945c4
kristina_greig <kristina.greig@eqtr.com>
2024-07-18 10:41:54 +0100

    update test

--- src/component-library/site-components/StickyCTA/StickyCTA.spec.tsx
+++ src/component-library/site-components/StickyCTA/StickyCTA.spec.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import StickyCTA from './StickyCTA';
 import { StickyCTAProps } from './StickyCTA.types';
+import Text from '../../foundation/Text/Text';
+import Button from '../../core-components/Button/Button';
 
 const mockProps: StickyCTAProps = {
-  children: <p>Hello world</p>,
+  cta: (
+    <Button size="large" variation="full">
+      <button>Book an appointment</button>
+    </Button>
+  ),
+  children: (
+    <Text tag="h2" variation="heading-1">
+      Start your journey now...
+    </Text>
+  ),
 };
 
 describe('StickyCTA', () => {
