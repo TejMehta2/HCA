a281b14752d083daf1aeac21184c2bb09250f13d
matthewhopkins-hcahealthcar <matthew.hopkins@hcahealthcare.co.uk>
2024-07-24 16:41:43 +0100

    proxy post

--- src/hcamain/src/components/EmailUtils/Log404Email.tsx
+++ src/hcamain/src/components/EmailUtils/Log404Email.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { ILogEmailFields, submitLogEmail } from 'lib/consultant-finder/API_HCA';
 import { useEffect } from 'react';
+import PostLogEmail from 'src/pages/api/formAPI/PostLogEmail';
 
 /**
  * Rendered in case we need to email for the 404
@@ -18,8 +20,35 @@ const postData = async (freeText: string) => {
     profileType: '404Report',
     freeText: freeText,
   };
-  //console.log('postData dataToSend', dataToSend);
-  /*const res = */ await submitLogEmail(dataToSend);
+  console.log('postData dataToSend', dataToSend);
+  axios
+    .post(`https:/api/formAPI/PostLogEmail`, dataToSend)
+    .then((resp) => {
+      console.log(resp?.data);
+      /*
+    setErrorData(false);
+    setLoadingData(false);
+    setInsurers(resp?.data?.insurers || []);
+    setPractices(resp?.data?.practices || []);
+    setConsulantName(
+      `${resp?.data?.title} ${resp?.data?.firstName} ${resp?.data?.lastName}`
+    );
+    // top specialty
+    const topSpecialty = resp?.data?.keywords?.filter(
+      (item: any) => item.parentName === 'ABSTRACT_TOP_LEVEL_KEYWORD'
+    );
+    setTopSpecialty(topSpecialty[0]?.name || '');*/
+    })
+    .catch((error) => {
+      /*
+    setErrorData(true);
+    setLoadingData(false);*/
+      console.log(error);
+    });
+
+  //await PostLogEmail(dataToSend);
+
+  /*const res = */ //await submitLogEmail(dataToSend);
   //console.log('postData res', res);
 };
 
