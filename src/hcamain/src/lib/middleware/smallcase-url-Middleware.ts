c728fdaa9f66c43e9d84af2b920c7a79c04ea46a
matthewhopkins-hcahealthcar <matthew.hopkins@hcahealthcare.co.uk>
2024-08-19 14:18:19 +0100

    lint

--- src/hcamain/src/lib/middleware/smallcase-url-Middleware.ts
+++ src/hcamain/src/lib/middleware/smallcase-url-Middleware.ts
@@ -1,13 +1,15 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-export function smallcaseurlMiddleware(req: NextRequest): NextResponse | undefined {
+export function smallcaseurlMiddleware(
+  req: NextRequest
+): NextResponse | undefined {
   const { pathname, origin, search } = req.nextUrl;
   const redirected = req.headers.get('x-redirected');
 
   // Check if the pathname is already in lowercase
   if (pathname === pathname.toLowerCase()) {
-    return;// NextResponse.next(); // Continue to the next middleware or route handler
+    return; // NextResponse.next(); // Continue to the next middleware or route handler
   }
 
   // Check if this request has already been redirected
