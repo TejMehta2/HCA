229dc844cc593b4e59ac9a4c0c9634367213ff36
ashley-eqtr <146321241+ashley-eqtr@users.noreply.github.com>
2024-08-20 13:35:56 +0100

    feature/HCA-682/careers-homepage-hero (#1054)
    * feature/HCA-682/careers-homepage-hero
    
    - Created new CareersHomepageHero
    - Created new YextStyledStaticFilters so we can use the logic from the OTB Yext filters, with custom styles.
    - Reused wrapper styles from homepagehero via import and documented it.
    - The filter behavior may change later once we have the Yext config set up e.g. they could behave as radios, it's difficult to predict so I will leave them as multi-select for now and tackle that during integration.
    - Adjusted search button so that it matches the height of the other CTA on default and careers HomepageHero on mobile.
    
    * feature/HCA-682/careers-homepage-hero
    
    * feature/HCA-682/careers-homepage-hero
    
    * feature/HCA-682/careers-homepage-hero

---
+++ src/component-library/careers/CareersHompageHero/CareersHompageHero.spec.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CareersHompageHero from './CareersHompageHero';
+import { CareersHompageHeroProps } from './CareersHompageHero.types';
+import SearchButton from '../../components/SearchButton/SearchButton';
+import Button from '../../core-components/Button/Button';
+import Image from 'next/image';
+import Text from '../../foundation/Text/Text';
+
+const mockProps: CareersHompageHeroProps = {
+  title: (
+    <Text tag="h1" variation="display-1">
+      Exceptional people. Delivering exceptional care.
+    </Text>
+  ),
+  search: (
+    <SearchButton onClick={() => {}}>
+      <span>
+        Search for a <b>keyword or role</b>
+      </span>
+    </SearchButton>
+  ),
+  filters: (
+    <>
+      <p>filter</p>
+    </>
+  ),
+  cta: (
+    <Button size={'large'} variation={'full'}>
+      <a href="#">Search roles</a>
+    </Button>
+  ),
+  image: (
+    <Image
+      src="/placeholders/couple-on-bench.jpeg"
+      alt="an old couple sitting on a bench having a nice time"
+      width={1460}
+      height={1460}
+    />
+  ),
+};
+
+describe('CareersHompageHero', () => {
+  it('Renders children from props', async () => {
+    const { getByText } = render(<CareersHompageHero {...mockProps} />);
+    expect(
+      getByText('Exceptional people. Delivering exceptional care.')
+    ).toBeVisible();
+  });
+});
