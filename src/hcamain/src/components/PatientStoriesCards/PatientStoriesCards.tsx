c751f0894d16a30fd670daaaf9943bcbf22a159a
Bartek Glomski <bartek.glomski@eqtr.com>
2024-10-02 13:44:29 +0200

    no stories check in patient stories slider

--- src/hcamain/src/components/PatientStoriesCards/PatientStoriesCards.tsx
+++ src/hcamain/src/components/PatientStoriesCards/PatientStoriesCards.tsx
@@ -346,6 +346,10 @@ export const Slider = (props: PatientStoriesCardsProps): JSX.Element => {
     patientStoriesCardsFiltered &&
     returnCards(props, patientStoriesCardsFiltered, false);
 
+  if (!patientStoriesCards?.length && !isExperienceEditor) {
+    return <></>;
+  }
+
   const viewAllCta = props?.fields?.data?.item?.patientStories
     ?.PatientStoriesList?.length
     ? props.fields?.data?.item?.cTALink?.jsonValue?.value?.href
