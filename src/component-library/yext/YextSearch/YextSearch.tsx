a8a4d108467e6452df8aca2f9e881fc34c2f89d7
Ella Koa <ella.koa@eqtr.com>
2024-10-07 09:24:33 +0100

    feature/HCA-682/toggle-vacancies
    - Continue fox for no results in search

--- src/component-library/yext/YextSearch/YextSearch.tsx
+++ src/component-library/yext/YextSearch/YextSearch.tsx
@@ -1,20 +1,13 @@
 import React, { useRef, useEffect } from 'react';
-import { UniversalResults, VerticalResults } from '@yext/search-ui-react';
+import { UniversalResults } from '@yext/search-ui-react';
 import styles from './YextSearch.module.scss';
 import YextTabs from '../YextTabs/YextTabs';
 import StyledYextSearchBar from '../StyledYextSearchBar/StyledYextSearchBar';
-import YextResultCardArticlesAdaptor from '../YextResultCardArticles/YextResultCardArticles.adaptor';
-import YextResultCardLinksAdaptor from '../YextResultCardLinks/YextResultCardLinks.adaptor';
-import YextResultCardFAQsAdaptor from '../YextResultCardFAQs/YextResultCardFAQs.adaptor';
+
 import YextCustomPagination from '../YextCustomPagination/YextCustomPagination';
-import YextResultSectionLocationsAdaptor from '../YextResultSectionLocations/YextResultSectionLocations.adaptor';
-import { Result, useSearchState } from '@yext/search-headless-react';
-import YextResultCardAskAQuestionAdaptor from '../YextResultCardAskAQuestion/YextResultCardAskAQuestion.adaptor';
-import YextResultCardConsultantsAdaptor from '../YextResultCardConsultants/YextResultCardConsultants.adaptor';
-import HealthcareFacility from '../../types/yext/healthcare_facilities';
-import YextResultCardDepartmentsAdaptor from '../YextResultCardDepartments/YextResultCardDepartments.adaptor';
-import YextResultCardTestsAndTreatmentsAdaptor from '../YextResultCardTestsAndTreatments/YextResultCardTestsAndTreatments.adaptor';
-import { VerticalKey } from './YextSearch.types';
+
+import { useSearchActions, useSearchState } from '@yext/search-headless-react';
+
 import { AlternativeVerticals } from '../YextCustomAlternativeVerticals/YextCustomAlternativeVerticals';
 import YextFiltersAdaptor from '../YextFilters/YextFilters.adaptor';
 import { ResultsCount } from '../YextCustomResultsCount/YextCustomResultsCount';
@@ -22,104 +15,44 @@ import Themes from '../../foundation/Themes/Themes';
 import { useRouter } from 'next/router';
 import YextNoResults from '../YextNoResults/YextNoResults';
 import { verticalConfigMap } from './verticalConfigMap';
-
-const Verticals = () => {
-  const searchState = useSearchState((state) => state);
-  const verticalKey = searchState.vertical.verticalKey as VerticalKey | 'links';
-  const results = searchState.vertical.results;
-
-  switch (verticalKey) {
-    case 'healthcare_facilities':
-      return (
-        <YextResultSectionLocationsAdaptor
-          results={(results as unknown as Result<HealthcareFacility>[]) || []}
-          variation={'side-by-side'}
-        />
-      );
-
-    case 'healthcare_professionals':
-      return (
-        <>
-          <VerticalResults
-            CardComponent={YextResultCardConsultantsAdaptor}
-            customCssClasses={{ verticalResultsContainer: styles.vertical }}
-          />
-        </>
-      );
-    case 'faqs':
-      return (
-        <>
-          <VerticalResults
-            CardComponent={YextResultCardFAQsAdaptor}
-            customCssClasses={{ verticalResultsContainer: styles.vertical }}
-          />
-          <YextResultCardAskAQuestionAdaptor />
-        </>
-      );
-    case 'tests_and_treatments':
-      return (
-        <VerticalResults
-          CardComponent={YextResultCardTestsAndTreatmentsAdaptor}
-          customCssClasses={{ verticalResultsContainer: styles.vertical }}
-        />
-      );
-
-    case 'articles':
-      return (
-        <VerticalResults
-          CardComponent={YextResultCardArticlesAdaptor}
-          customCssClasses={{ verticalResultsContainer: styles.vertical }}
-        />
-      );
-    case 'specialties':
-      return (
-        <VerticalResults
-          CardComponent={YextResultCardDepartmentsAdaptor}
-          customCssClasses={{ verticalResultsContainer: styles.vertical }}
-        />
-      );
-    case 'links':
-      return (
-        <VerticalResults
-          CardComponent={YextResultCardLinksAdaptor}
-          customCssClasses={{ verticalResultsContainer: styles.vertical }}
-        />
-      );
-    default:
-      return <></>;
-  }
-};
+import Verticals from './Verticals';
 
 const YextSearch = (): JSX.Element => {
   const searchQuery = useSearchState((state) => state.query.input);
+  const searchActions = useSearchActions();
   const router = useRouter();
-
   useEffect(() => {
-    if (!searchQuery) return;
-    if (router.isReady) {
-      const searchParams = new URLSearchParams(location.search);
-      searchParams.set('query', searchQuery);
-      router.push(
-        {
-          pathname: location.pathname,
-          query: searchParams.toString(),
-        },
-        undefined,
-        { shallow: true }
-      );
+    const searchParams = new URLSearchParams(location.search);
+
+    if (searchQuery) {
+      if (router.isReady) {
+        searchParams.set('query', searchQuery);
+        router.push(
+          {
+            pathname: location.pathname,
+            query: searchParams.toString(),
+          },
+          undefined,
+          { shallow: true }
+        );
+      }
+    } else {
+      const query = searchParams.get('query');
+      searchActions.setQuery(query || '');
+      searchActions.executeUniversalQuery();
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchQuery, router.isReady]);
+  }, [searchQuery]);
 
   const resultsCountRef = useRef<HTMLDivElement>(null);
   const verticalKey = useSearchState((state) => state.vertical.verticalKey);
   const isLoading = useSearchState((state) => state.searchStatus.isLoading);
 
-  const verticalResults = useSearchState((state) => state.universal?.verticals);
-
-  const verticalResultsLength = verticalResults
-    ? verticalResults && verticalResults?.length
-    : 0;
+  const universalResults = useSearchState((state) => state.universal.verticals);
+  const universalResultsLength = universalResults?.length || 0;
+  const verticalResultsLength =
+    useSearchState((state) => state.vertical.resultsCount) || 0;
 
   return (
     <div className={styles.wrapper}>
@@ -137,7 +70,7 @@ const YextSearch = (): JSX.Element => {
           </div>
         </div>
 
-        {verticalResultsLength > 0 && !isLoading ? (
+        {universalResultsLength > 0 && !isLoading ? (
           <UniversalResults verticalConfigMap={verticalConfigMap} />
         ) : (
           searchQuery !== '' &&
@@ -149,22 +82,25 @@ const YextSearch = (): JSX.Element => {
             />
           )
         )}
-        {verticalKey && verticalKey !== 'healthcare_facilities' && (
-          <Themes theme={'O-HCA-Teal-20'}>
-            <AlternativeVerticals
-              currentVerticalLabel={
-                verticalConfigMap[
-                  verticalKey as
-                    | 'articles'
-                    | 'tests_and_treatments'
-                    | 'specialties'
-                    | 'faqs'
-                ]?.label || ''
-              }
-              verticalConfigMap={verticalConfigMap}
-            />
-          </Themes>
-        )}
+        {!isLoading &&
+          verticalKey &&
+          verticalKey !== 'healthcare_facilities' &&
+          verticalResultsLength <= 0 && (
+            <Themes theme={'O-HCA-Teal-20'}>
+              <AlternativeVerticals
+                currentVerticalLabel={
+                  verticalConfigMap[
+                    verticalKey as
+                      | 'articles'
+                      | 'tests_and_treatments'
+                      | 'specialties'
+                      | 'faqs'
+                  ]?.label || ''
+                }
+                verticalConfigMap={verticalConfigMap}
+              />
+            </Themes>
+          )}
         <Verticals />
         <YextCustomPagination
           callback={() => {
