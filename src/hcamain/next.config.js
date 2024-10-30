2cba003eebc2e6e499029945b7b661789eba99a7
Bartek Glomski <bartek.glomski@eqtr.com>
2024-10-30 12:51:14 +0100

    Remove "X-Powered-By: Next.js" Response header

--- src/hcamain/next.config.js
+++ src/hcamain/next.config.js
@@ -21,6 +21,9 @@ const securityHeaders = [
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  // Disable "X-Powered-By: Next.js" Response header
+  poweredByHeader: false,
+
   // Set assetPrefix to our public URL
   assetPrefix: publicUrl,
 
