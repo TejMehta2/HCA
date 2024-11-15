54ec839fb460a4b8ab4f9347509597c291f66ac0
Ella Koa <ella.koa@eqtr.com>
2024-11-15 10:46:42 +0000

    feature/HED-1742/quote-component

--- src/component-library/site-components/QuoteBlockCenter/QuoteBlockCenter.spec.tsx
+++ src/component-library/site-components/QuoteBlockCenter/QuoteBlockCenter.spec.tsx
@@ -4,12 +4,21 @@ import QuoteBlockCenter from './QuoteBlockCenter';
 import { QuoteBlockCenterProps } from './QuoteBlockCenter.types';
 
 const mockProps: QuoteBlockCenterProps = {
-  children: <p>Hello world</p>,
+  children: <p>children</p>,
+  author: {
+    name: <p>author</p>,
+    image: <p>image</p>,
+    tag: <p>tag</p>,
+  },
+  alignment: 'center',
 };
 
 describe('QuoteBlockCenter', () => {
   it('Renders children from props', async () => {
     const { getByText } = render(<QuoteBlockCenter {...mockProps} />);
-    expect(getByText('Hello world')).toBeVisible();
+    expect(getByText('children')).toBeVisible();
+    expect(getByText('author')).toBeVisible();
+    expect(getByText('image')).toBeVisible();
+    expect(getByText('tag')).toBeVisible();
   });
 });
