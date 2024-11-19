0c6242e8fc9b7d776700ae7ebc957b6c3818ef1a
Bartek Glomski <bartek.glomski@eqtr.com>
2024-11-20 00:40:24 +0100

    be + base subnav integration

---
+++ src/hcamain/src/components/SubNavigation/SubNavigation.types.tsx
@@ -0,0 +1,33 @@
+import { Field, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
+import Params from 'src/types/params';
+
+export interface Fields {
+  data?: {
+    item?: {
+      title?: { jsonValue?: Field<string> };
+      defaultNavigationImage?: { jsonValue: ImageField };
+      rootPage?: {
+        targetItem?: {
+          children?: {
+            results?: NavigablePagesFields[];
+          };
+        };
+      };
+    };
+  };
+}
+
+export interface NavigablePagesFields {
+  navigationTitle?: { value?: string };
+  abstractTitle?: { value?: string };
+  abstractImage?: { jsonValue: ImageField };
+  title?: { value?: string };
+  image?: { jsonValue: ImageField };
+  url?: { path?: string };
+  hideInSubNavigation: { boolValue: boolean };
+}
+
+export interface SubNavigationProps {
+  params?: Params;
+  fields?: Fields;
+}
