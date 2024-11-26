5ee62d1c2df451dcca6a187afe7f719b0cd69d6c
Ella Koa <ella.koa@eqtr.com>
2024-11-26 08:57:36 +0000

    feature/HED-1809/Standalone-Accordions
    - Refactor for maintainability

--- src/component-library/site-components/AccordionsStandalone/AccordionsStandalone.types.ts
+++
@@ -1,7 +0,0 @@
-import { Accordions } from '../../components/Accordions/Accordions.types';
-import { Theme } from '../../foundation/Themes/Themes.types';
-
-export interface AccordionsStandaloneProps {
-  theme: Theme;
-  accordions: Accordions;
-}
