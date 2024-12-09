2dafc9feb369c0ccff6538c62cab0e477dc46e9b
alinaHCA <Alina.Antemir@hcahealthcare.co.uk>
2024-12-09 12:09:04 +0000

    test redirect

--- src/hcamain/src/lib/redirect-middleware.ts
+++ src/hcamain/src/lib/redirect-middleware.ts
@@ -2,38 +2,50 @@ import { PHASE_PRODUCTION_BUILD } from 'next/dist/shared/lib/constants';
 import { NextRequest, NextResponse } from 'next/server';
 
 const redirectMiddleware = async (req: NextRequest) => {
-  //console.error('process.env.NEXT_PHASE : ', process.env.NEXT_PHASE);
-  //console.error('PHASE_PRODUCTION_BUILD : ', PHASE_PRODUCTION_BUILD);
   if (process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD) {
     try {
       // Custom redirect if response from API is valid and has a destination
       const { url } = req;
       const { pathname, search } = new URL(url);
-      //console.error('url : ', url);
 
-      const apiUrl = new URL(
-        `${process.env.INTEGRATION_LAYER_URL}/redirects/find?source=${pathname}`
+      console.log(
+        `redirect 1: url ${url}, pathname: ${pathname}, search: ${search}`
       );
 
-      //console.log('apiUrl.href', apiUrl.href);
+      const apiUrl = new URL(
+        //`${process.env.INTEGRATION_LAYER_URL}/redirects/find?source=${pathname}`
+        `https://www.hcahealthcare.co.uk/api/api-layer/redirects/find?source=${pathname}`
+      );
+
+      console.log(`redirect 2: apiUrl ${JSON.stringify(apiUrl)}`);
       const response = await fetch(apiUrl.href, { next: { revalidate: 3600 } });
+      console.log(`redirect 3: ok?${response.ok}`);
+
       if (response.ok) {
-        //console.log('response', response.ok);
-        const data = await response.json();
-        //console.log('response.json', response.json);
-        if (data.destination) {
-          //console.log('data.destination', data.destination);
-          const redirectUrl = new URL(data.destination, req.url);
-          redirectUrl.search = search;
-          //console.log('redirectUrl', redirectUrl);
-          return NextResponse.redirect(redirectUrl);
+        //&& (await response.bodyUsed)
+        const text1 = await response.text();
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let data: any = {};
+        if (text1.length > 0) {
+          data = JSON.parse(text1);
+          console.log('data', data);
+          //const data = await response.json();
+          if (data.destination) {
+            const redirectUrl = new URL(data.destination, req.url);
+            redirectUrl.search = search;
+            console.log(
+              `redirect 4: url ${url}, pathname: ${pathname},  search: ${search}`
+            );
+            return NextResponse.redirect(redirectUrl);
+          }
         }
       } else {
-        console.log('response.statusText', response.statusText);
+        console.log(`redirect 5:`, JSON.stringify(response));
         throw response.statusText;
       }
     } catch (error) {
-      console.error('Redirect Middleware : ', error);
+      process.env.NODE_ENV === 'development' &&
+        console.error('Redirect Middleware : ', error);
     }
   }
   return;
