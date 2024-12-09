35159be43f27d07982224308cf7f80e636c819f0
Matthew Hopkins <Matthew.Hopkins@HCAHealthcare.co.uk>
2024-12-09 09:17:52 +0000

    remove console debug from middleware

--- src/hcamain/src/lib/redirect-middleware.ts
+++ src/hcamain/src/lib/redirect-middleware.ts
@@ -2,30 +2,30 @@ import { PHASE_PRODUCTION_BUILD } from 'next/dist/shared/lib/constants';
 import { NextRequest, NextResponse } from 'next/server';
 
 const redirectMiddleware = async (req: NextRequest) => {
-  console.error('process.env.NEXT_PHASE : ', process.env.NEXT_PHASE);
-  console.error('PHASE_PRODUCTION_BUILD : ', PHASE_PRODUCTION_BUILD);
+  //console.error('process.env.NEXT_PHASE : ', process.env.NEXT_PHASE);
+  //console.error('PHASE_PRODUCTION_BUILD : ', PHASE_PRODUCTION_BUILD);
   if (process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD) {
     try {
       // Custom redirect if response from API is valid and has a destination
       const { url } = req;
       const { pathname, search } = new URL(url);
-      console.error('url : ', url);
+      //console.error('url : ', url);
 
       const apiUrl = new URL(
         `${process.env.INTEGRATION_LAYER_URL}/redirects/find?source=${pathname}`
       );
 
-      console.log('apiUrl.href', apiUrl.href);
+      //console.log('apiUrl.href', apiUrl.href);
       const response = await fetch(apiUrl.href, { next: { revalidate: 3600 } });
       if (response.ok) {
-        console.log('response', response.ok);
+        //console.log('response', response.ok);
         const data = await response.json();
-        console.log('response.json', response.json);
+        //console.log('response.json', response.json);
         if (data.destination) {
-          console.log('data.destination', data.destination);
+          //console.log('data.destination', data.destination);
           const redirectUrl = new URL(data.destination, req.url);
           redirectUrl.search = search;
-          console.log('redirectUrl', redirectUrl);
+          //console.log('redirectUrl', redirectUrl);
           return NextResponse.redirect(redirectUrl);
         }
       } else {
