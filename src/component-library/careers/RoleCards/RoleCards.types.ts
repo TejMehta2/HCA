44575127d39ad196f03fc188eeb740eb8ac5715e
MichaelRendall <michael.rendall@eqtr.com>
2025-03-11 12:48:05 +0000

    service cards careers variation

--- src/component-library/careers/RoleCards/RoleCards.types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface RoleCardsProps {
-  children?: JSX.Element[];
-  title: ReactNode | JSX.Element;
-  subtitle: ReactNode | JSX.Element;
-  bodyText: ReactNode | JSX.Element;
-  id?: string;
-}
