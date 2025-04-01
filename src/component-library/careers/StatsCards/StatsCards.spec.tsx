34eec9247e17f23bafd79811696964871fbc94b7
MichaelRendall <michael.rendall@eqtr.com>
2025-04-01 10:00:38 +0100

    fix unit tests

--- src/component-library/careers/StatsCards/StatsCards.spec.tsx
+++ src/component-library/careers/StatsCards/StatsCards.spec.tsx
@@ -5,6 +5,20 @@ import { StatsCardsProps } from './StatsCards.types';
 import Text from '../../foundation/Text/Text';
 import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 const mockProps: StatsCardsProps = {
   theme: 'K-HCA-Fern-20',
   header: (
