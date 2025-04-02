39bae47430e769d9e2125d7ef5091aed7ec96ad8
kristina_greig <kristina.greig@eqtr.com>
2025-04-02 15:40:53 +0100

    add new chelsea themes

--- src/component-library/core-components/Switches/Switches.types.ts
+++ src/component-library/core-components/Switches/Switches.types.ts
@@ -1,5 +1,12 @@
 export interface SwitchesProps {
   label: string;
-  mode?: 'light' | 'dark' | 'medium' | 'palace-light' | 'palace-dark';
+  mode?:
+    | 'light'
+    | 'dark'
+    | 'medium'
+    | 'palace-light'
+    | 'palace-dark'
+    | 'chelsea-light'
+    | 'chelsea-dark';
   disabled?: boolean;
 }
