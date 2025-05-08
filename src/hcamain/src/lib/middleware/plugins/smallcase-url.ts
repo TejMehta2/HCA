8a1f1df5489ac204f844c94e6a3a17c49198f121
Bartek Glomski <bartek.glomski@eqtr.com>
2025-05-08 18:16:53 +0200

    logs

--- src/hcamain/src/lib/middleware/plugins/smallcase-url.ts
+++ src/hcamain/src/lib/middleware/plugins/smallcase-url.ts
@@ -7,18 +7,27 @@ import { MiddlewarePlugin } from '..';
 class SmallcaseUrlPlugin implements MiddlewarePlugin {
   order = -2; // Run before multisite, as it normalizes the URL
 
-  async exec(req: NextRequest): Promise<NextResponse> {
+  async exec(req: NextRequest, res?: NextResponse): Promise<NextResponse> {
     const url = req.nextUrl.clone();
     const pathname = url.pathname;
 
+    console.log(
+      `Smallcase redirect. order=${this.order}: Checking for redirect - url=${url}, pathname=${pathname}`
+    );
+
     if (pathname !== pathname.toLowerCase()) {
       url.pathname = pathname.toLowerCase();
+
+      console.log(
+        `Smallcase redirect: will redirect to  - url.pathname=${url.pathname}`
+      );
 
       // Rewriting to lowercase version of the path
       return NextResponse.redirect(url);
     }
 
-    return NextResponse.next();
+    console.log(`Smallcase redirect: moving to the next plugin`);
+    return res || NextResponse.next();
   }
 }
 
