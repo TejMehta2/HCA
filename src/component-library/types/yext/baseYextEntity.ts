3566ed3b73fc781a09b2c84d34ea7b495e727a96
Bartek Glomski <bartek.glomski@eqtr.com>
2025-07-30 12:04:05 +0200

    adding more props to base interface

--- src/component-library/types/yext/baseYextEntity.ts
+++ src/component-library/types/yext/baseYextEntity.ts
@@ -1,3 +1,12 @@
 export interface BaseYextEntity {
   c_abstractTitle: string;
+  c_abstractText: string;
+  c_pageTitle: string;
+  c_pageText: string;
+  c_nameRichText?: string;
+  name: string;
+  id: string;
+  c_uRL?: string;
+  c_body?: string;
+  c_fullBodyText?: string;
 }
