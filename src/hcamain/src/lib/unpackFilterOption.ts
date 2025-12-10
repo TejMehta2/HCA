dd76aef56a2abf4d8569ead5949174c02257efa8
Bartek Glomski <bartek.glomski@eqtr.com>
2025-12-11 00:55:35 +0100

    lint

--- src/hcamain/src/lib/unpackFilterOption.ts
+++ src/hcamain/src/lib/unpackFilterOption.ts
@@ -5,10 +5,7 @@ const formatId = (id?: string) => id?.toLowerCase()?.replace(/[-{}]/g, '');
 const unpackFilterOption = ({ fields }: FilterOption) => {
   const { Filter, FilterValueGuid, FilterValueString, DisplayName } = fields;
   const key = Filter?.value || '';
-  const value =
-    formatId(FilterValueGuid) ||   
-    FilterValueString.value ||
-    '';
+  const value = formatId(FilterValueGuid) || FilterValueString.value || '';
   return {
     id: `${key}-${value}`,
     key,
