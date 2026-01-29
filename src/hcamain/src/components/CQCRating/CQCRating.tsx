b65bdfca1436d135cee11dba5f57669101ff6c1f
Bartek Glomski <bartek.glomski@eqtr.com>
2026-01-29 14:05:28 +0100

    checking editing context and fixing link check

--- src/hcamain/src/components/CQCRating/CQCRating.tsx
+++ src/hcamain/src/components/CQCRating/CQCRating.tsx
@@ -2,21 +2,34 @@ import React from 'react';
 import {
   Image as JssImage,
   Link as JssLink,
+  useSitecoreContext,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import Icons from '@component-library/foundation/Icons/Icons';
 import CQCBlock from '@component-library/components/CQCBlock/CQCBlock';
 import { CQCBlockProps } from '@component-library/components/CQCBlock/CQCBlock.types';
 import { CQCRatingProps } from './CQCRating.types';
 
-const CQCRatingDefaultComponent = (props: CQCRatingProps): JSX.Element => (
-  <div className={`component ${props.params?.styles}`}>
-    <div className="component-content">
-      <span className="is-empty-hint">CQC Rating no datasource</span>
+const CQCRatingDefaultComponent = (props: CQCRatingProps): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
+  const isExperienceEditor = sitecoreContext.pageEditing;
+
+  return !isExperienceEditor ? (
+    <></>
+  ) : (
+    <div className={`component ${props.params?.styles}`}>
+      <div className="component-content">
+        <span className="is-empty-hint">
+          CQC Ratings. Please click to select datasource.
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export const Default = (props: CQCRatingProps): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
+  const isExperienceEditor = sitecoreContext.pageEditing;
+
   if (!props.fields) {
     return <CQCRatingDefaultComponent {...props} />;
   }
@@ -29,8 +42,8 @@ export const Default = (props: CQCRatingProps): JSX.Element => {
   ) as CQCBlockProps['rating'];
 
   const ratingLink =
-    props.fields?.ReportLink && 'url' in props.fields?.ReportLink ? (
-      <a href={props.fields?.ReportLink?.url}></a>
+    props.fields?.ReportLink?.value?.href && !isExperienceEditor ? (
+      <a href={props.fields?.ReportLink?.value?.href}></a>
     ) : (
       props.fields?.ReportLink && <JssLink field={props.fields?.ReportLink} />
     );
