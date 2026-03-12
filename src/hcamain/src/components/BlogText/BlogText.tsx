8c0c9736abe3aa12f1240101596360d0e852b091
Bartek Glomski <bartek.glomski@eqtr.com>
2026-03-12 14:34:35 +0100

    parsing h2 tags in blog text so that they are picked up by ToC

--- src/hcamain/src/components/BlogText/BlogText.tsx
+++ src/hcamain/src/components/BlogText/BlogText.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   Field,
   RichText as JssRichText,
@@ -9,6 +8,7 @@ import BlogContent from '@component-library/site-components/BlogContent/BlogCont
 import Params from 'src/types/params';
 import RichText from '@component-library/core-components/RichText/RichText';
 import { inPageNavGlobalStore } from '../../context/inPageNavGlobalStorage';
+import { generateHtmlSafeId } from 'lib/utility-functions/generateHtmlSafeId';
 
 interface Fields {
   Text?: Field<string>;
@@ -37,6 +37,9 @@ const BlogTextDefaultComponent = (props: BlogTextProps): JSX.Element => {
 };
 
 export const Default = (props: BlogTextProps): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
+  const isExperienceEditor = sitecoreContext.pageEditing;
+
   if (!props.fields) {
     return <BlogTextDefaultComponent {...props} />;
   }
@@ -44,26 +47,45 @@ export const Default = (props: BlogTextProps): JSX.Element => {
   const componentAnchorId = inPageNavGlobalStore.addItem(props?.params, '');
   const tableOfContentTitle = props?.params?.TableOfContentsLinkTitle;
 
-  const isContainerized = props?.params?.Containerized === '1';
+  // Update H2 tag in the Rich Text field value if not in Editing mode
+  const processedField = useMemo(() => {
+    const rawValue = props.fields?.Text?.value;
 
-  if (isContainerized) {
-    return (
-      <RichText additionalStyles={props?.params?.styles} id={componentAnchorId}
-        {...(tableOfContentTitle && props?.params?.ExcludeFromTableOfContents !== '1' ? { tableOfContentTitle: tableOfContentTitle } : {})}
-      >
-        <JssRichText field={props.fields?.Text} />
-      </RichText>
+    if (
+      isExperienceEditor ||
+      !rawValue ||
+      props.params?.ExtractH2Links !== '1'
+    ) {
+      return props.fields?.Text;
+    }
+
+    const transformedValue = rawValue.replace(
+      /<h2[^>]*>(.*?)<\/h2>/gi,
+      (_match, fullContent) => {
+        const cleanText = fullContent.replace(/<[^>]*>/g, '').trim();
+        const cleanId = generateHtmlSafeId(cleanText);
+        //h2 format required by TOC
+        return `<h2 id="${cleanId}" data-subnav-link-title="${cleanText}">${fullContent}</h2>`;
+      }
     );
-  }
+
+    return {
+      ...props.fields?.Text,
+      value: transformedValue,
+    };
+  }, [props.fields?.Text, isExperienceEditor]);
 
   return (
     <BlogContent
       theme={props.params?.Theme || 'A-HCA-White'}
       id={componentAnchorId}
-      {...(tableOfContentTitle && props?.params?.ExcludeFromTableOfContents !== '1' ? { tableOfContentTitle: tableOfContentTitle } : {})}
+      {...(tableOfContentTitle &&
+      props?.params?.ExcludeFromTableOfContents !== '1'
+        ? { tableOfContentTitle: tableOfContentTitle }
+        : {})}
     >
       <RichText>
-        <JssRichText field={props.fields?.Text} />
+        <JssRichText field={processedField} />
       </RichText>
     </BlogContent>
   );
