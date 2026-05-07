4bc22a0caee54c8247695a590d51f50bcd36c85e
Bartek Glomski <bartek.glomski@eqtr.com>
2026-05-07 16:12:27 +0200

    adding Theme to the app layout

--- src/hcamain-contentsdk/src/app/layout.tsx
+++ src/hcamain-contentsdk/src/app/layout.tsx
@@ -1,9 +1,17 @@
 import './globals.css';
+import '@component-library/globals/index.scss';
+import Themes from '@component-library/foundation/Themes/Themes';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>      
+        <Themes theme={'A-HCA-White'}>{children}</Themes>
+      </body>
     </html>
   );
 }
