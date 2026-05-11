7d7614fdf7a904110ad5ba99934d302a6c0a35e8
Bartek Glomski <bartek.glomski@eqtr.com>
2026-05-11 12:32:30 +0200

    moving theme mapping from scss to ts

--- src/component-library-contentsdk/foundation/Themes/Themes.tsx
+++ src/component-library-contentsdk/foundation/Themes/Themes.tsx
@@ -3,6 +3,38 @@ import { ThemesProps } from './Themes.types';
 import styles from './Themes.module.scss';
 import { generateHtmlSafeId } from '../../utility-functions/index';
 
+const themeModes: Record<string, 'dark' | 'light'> = {
+  'A-HCA-White': 'dark',
+  'B-HCA-Navy-Blue': 'light',
+  'C-HCA-Denim': 'light',
+  'D-HCA-Teal': 'dark',
+  'E-HCA-Cerulean': 'dark',
+  'F-HCA-Fern': 'dark',
+  'G-HCA-Orange': 'dark',
+  'H-HCA-Tangerine': 'dark',
+  'I-HCA-Goldenrod': 'dark',
+  'J-HCA-Tangerine-20': 'dark',
+  'K-HCA-Fern-20': 'dark',
+  'L-HCA-Teal-5': 'dark',
+  'M-HCA-Goldenrod-20': 'dark',
+  'N-HCA-Denim-5': 'dark',
+  'O-HCA-Teal-20': 'dark',
+  'Palace-White': 'dark',
+  'Palace-Grey': 'light',
+  'Palace-Beige': 'dark',
+  'Palace-Red': 'light',
+  'Chelsea-White': 'dark',
+  'Chelsea-Navy-Blue': 'light',
+  'Chelsea-Beige': 'dark',
+  'Chelsea-Gold': 'dark',
+  LBI: 'dark',
+  'LBI-Dark': 'dark',
+  'LBI-White': 'light',
+  'Alan-Black': 'light',
+  'Alan-White': 'dark',
+  'Alan-Light-Grey': 'dark',
+};
+
 const Themes = (props: ThemesProps): JSX.Element => {
   const {
     children,
@@ -33,7 +65,7 @@ const Themes = (props: ThemesProps): JSX.Element => {
         collapse ? styles.collapse : styles['no-collapse'],
       ].join(' ')}
       data-theme={theme}
-      data-theme-mode={styles?.[`mode-${theme}`]}
+      data-theme-mode={themeModes[theme]}
       {...(tableOfContentTitle ? { 'data-subnav-link-title': linkTableOfContentTitle } : {})}
       {...(tableOfContentTitle ? { 'data-subnav-link-id': linkTableOfContentId } : {})}
     >
