8e9084596a9f0c6ef1142725eadbf17b41dfc417
Bartek Glomski <bartek.glomski@eqtr.com>
2026-06-04 13:40:38 +0200

    porting icons to site layout

--- src/hcamain-contentsdk/src/app/[site]/layout.tsx
+++ src/hcamain-contentsdk/src/app/[site]/layout.tsx
@@ -1,5 +1,40 @@
 import { draftMode } from 'next/headers';
+import type { Metadata } from 'next';
 import Bootstrap from 'src/Bootstrap';
+
+export const metadata: Metadata = {
+  icons: {
+    apple: {
+      url: '/favicon/apple-touch-icon.png',
+      sizes: '180x180',
+    },
+    icon: [
+      {
+        url: '/favicon/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+    ],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/favicon/safari-pinned-tab.svg',
+        color: '#ffffff',
+      },
+      {
+        rel: 'mask-icon',
+        url: '/favicon/safari-pinned-tab.svg',
+        color: '#00558c',
+      },
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
+};
 
 export default async function SiteLayout({
   children,
